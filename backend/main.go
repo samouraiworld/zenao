f9bbb16120ccfa5b6cb80b3d567a8e5020655a72
Norman <norman@samourai.coop>
2025-01-15 21:07:56 +0100

    feat(backend): add request logger
    Signed-off-by: Norman <norman@samourai.coop>

--- backend/main.go
+++ backend/main.go
@@ -6,17 +6,26 @@ import (
 	connectcors "connectrpc.com/cors"
 	"github.com/rs/cors"
 	"github.com/samouraiworld/zenao/backend/zenao/v1/zenaov1connect"
+	"go.uber.org/zap"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+
 	zenao := &ZenaoServer{}
 	mux := http.NewServeMux()
 	path, handler := zenaov1connect.NewZenaoServiceHandler(zenao)
-	mux.Handle(path, withCORS(handler))
+	mux.Handle(path, withLogging(withCORS(handler), logger))
+
+	addr := "localhost:4242"
+	logger.Info("Starting server", zap.String("addr", addr))
 	http.ListenAndServe(
-		"localhost:4242",
+		addr,
 		// Use h2c so we can serve HTTP/2 without TLS.
 		h2c.NewHandler(mux, &http2.Server{}),
 	)
@@ -30,4 +39,15 @@ func withCORS(h http.Handler) http.Handler {
 		ExposedHeaders: connectcors.ExposedHeaders(),
 	})
 	return middleware.Handler(h)
+}
+
+func withLogging(next http.Handler, logger *zap.Logger) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger.Info("Request",
+			zap.String("method", r.Method),
+			zap.String("path", r.RequestURI),
+			zap.String("host", r.Host),
+		)
+		next.ServeHTTP(w, r)
+	})
 }
--- go.mod
+++ go.mod
@@ -6,8 +6,12 @@ require (
 	connectrpc.com/connect v1.18.1
 	connectrpc.com/cors v0.1.0
 	github.com/rs/cors v1.11.1
+	go.uber.org/zap v1.27.0
 	golang.org/x/net v0.23.0
 	google.golang.org/protobuf v1.36.3
 )
 
-require golang.org/x/text v0.14.0 // indirect
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
