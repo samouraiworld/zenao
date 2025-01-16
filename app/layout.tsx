a992e2335d9f90af97e4043a29566d51d51b96ad
clegirar <clemntgirard@gmail.com>
2025-01-16 10:56:30 +0100

    fix: remove unused imports
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,7 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ToggleTheme } from "@/app/components/ToggleTheme";
 import { ThemeProvider } from "@/app/components/theme-provider";
 
 const geistSans = Geist({
