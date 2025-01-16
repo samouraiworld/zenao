286b1447e6a2d550b61813117fe579a536348758
clegirar <clemntgirard@gmail.com>
2025-01-16 13:18:40 +0100

    fix: comment related issue that propose a fix for hydration error
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- app/components/theme-provider.tsx
+++ app/components/theme-provider.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import * as React from "react";
+import dynamic from "next/dynamic";
+
+// https://github.com/shadcn-ui/ui/issues/5552#issuecomment-2435053678
+// Related issue that propose a fix for hydration error
+
 const NextThemesProvider = dynamic(
   () => import("next-themes").then((e) => e.ThemeProvider),
   {
     ssr: false,
   },
 );
-
-import dynamic from "next/dynamic";
 
 export function ThemeProvider({
   children,
