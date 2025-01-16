8e8f9b3e180df5d1d37690b199335b44ab96d7ca
clegirar <33428384+clegirar@users.noreply.github.com>
2025-01-16 13:22:18 +0100

    fix: hydration error for ThemeProvider (#31)
    * fix: hydration error for ThemeProvider
    
    Signed-off-by: clegirar <clemntgirard@gmail.com>
    
    * fix: correctly fix hydration error and disable ssr for next-themes
    
    Signed-off-by: clegirar <clemntgirard@gmail.com>
    
    * fix: comment related issue that propose a fix for hydration error
    
    Signed-off-by: clegirar <clemntgirard@gmail.com>
    
    ---------
    
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- app/components/theme-provider.tsx
+++ app/components/theme-provider.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import dynamic from "next/dynamic";
+
+// https://github.com/shadcn-ui/ui/issues/5552#issuecomment-2435053678
+// Related issue that propose a fix for hydration error
+
+const NextThemesProvider = dynamic(
+  () => import("next-themes").then((e) => e.ThemeProvider),
+  {
+    ssr: false,
+  },
+);
 
 export function ThemeProvider({
   children,
