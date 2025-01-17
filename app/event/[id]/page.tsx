7d7f9607e61269ddc8f36cb5663c8e101fd36feb
Norman <norman@samourai.coop>
2025-01-17 15:51:13 +0100

    fix: type def
    Signed-off-by: Norman <norman@samourai.coop>

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -5,10 +5,14 @@ import { Footer } from "@/components/layout/Footer";
 import { eventOptions } from "@/lib/queries/event";
 import { getQueryClient } from "@/lib/get-query-client";
 
-export default function EventPage({ params }: { params: { id: string } }) {
-  const id = params.id;
+export default async function EventPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const p = await params;
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(eventOptions(id));
+  void queryClient.prefetchQuery(eventOptions(p.id));
   return (
     <div className="h-screen flex flex-col font-[family-name:var(--font-geist-sans)]">
       <Header />
@@ -16,7 +20,7 @@ export default function EventPage({ params }: { params: { id: string } }) {
         <main className="flex flex-col mb-10 justify-center items-center">
           <div className="flex gap-4 mt-8 items-center flex-col sm:flex-row">
             <HydrationBoundary state={dehydrate(queryClient)}>
-              <EventInfo id={id} />
+              <EventInfo id={p.id} />
             </HydrationBoundary>
           </div>
         </main>
