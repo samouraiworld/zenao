b2b16595ec699f6a1b9766f7be69ae6557fb6689
Norman <norman@samourai.coop>
2025-01-17 15:48:25 +0100

    feat: slug-based event page
    Signed-off-by: Norman <norman@samourai.coop>

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -5,10 +5,10 @@ import { Footer } from "@/components/layout/Footer";
 import { eventOptions } from "@/lib/queries/event";
 import { getQueryClient } from "@/lib/get-query-client";
 
-export default function EventPage() {
-  const eventID = "1";
+export default function EventPage({ params }: { params: { id: string } }) {
+  const id = params.id;
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(eventOptions(eventID));
+  void queryClient.prefetchQuery(eventOptions(id));
   return (
     <div className="h-screen flex flex-col font-[family-name:var(--font-geist-sans)]">
       <Header />
@@ -16,7 +16,7 @@ export default function EventPage() {
         <main className="flex flex-col mb-10 justify-center items-center">
           <div className="flex gap-4 mt-8 items-center flex-col sm:flex-row">
             <HydrationBoundary state={dehydrate(queryClient)}>
-              <EventInfo id={eventID} />
+              <EventInfo id={id} />
             </HydrationBoundary>
           </div>
         </main>
