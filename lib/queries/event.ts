0f76e91d7859669ec5578cffb3c5e87a762425a7
clegirar <clemntgirard@gmail.com>
2025-01-26 16:29:13 +0100

    feat: generate metadata for event page
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- lib/queries/event.ts
+++ lib/queries/event.ts
@@ -78,7 +78,7 @@ export const eventCountParticipants = (id: string) =>
     },
   });
 
-function extractGnoJSONResponse(res: string): unknown {
+export function extractGnoJSONResponse(res: string): unknown {
   const jsonString = res.substring("(".length, res.length - " string)".length);
   // eslint-disable-next-line no-restricted-syntax
   const jsonStringContent = JSON.parse(jsonString);
