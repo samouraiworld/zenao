5369d3fdf8772a1d46d8720ba9e2d6a4e40df1d0
clegirar <clemntgirard@gmail.com>
2025-01-27 15:20:20 +0100

    fix: setup eslint no-unused-vars rule
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- app/hooks/use-toast.ts
+++ app/hooks/use-toast.ts
@@ -15,8 +15,7 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const actionTypes = {
+const _actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
   DISMISS_TOAST: "DISMISS_TOAST",
@@ -30,7 +29,7 @@ function genId() {
   return count.toString();
 }
 
-type ActionType = typeof actionTypes;
+type ActionType = typeof _actionTypes;
 
 type Action =
   | {
