3e8ce0186f32baf92bb39302694b5ec59ef7169c
clegirar <clemntgirard@gmail.com>
2025-02-06 09:36:35 +0100

    fix: check if the user is logged in and not banned
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -1,8 +1,21 @@
 import { NextResponse, type NextRequest } from "next/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { pinata } from "@/lib/pinata";
 
 export async function POST(request: NextRequest) {
   try {
+    const { userId } = await auth();
+    // User not logged in
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const client = await clerkClient();
+    const user = await client.users.getUser(userId);
+    // User banned
+    if (user.banned) {
+      return NextResponse.json({ error: "Banned" }, { status: 403 });
+    }
+
     const data = await request.formData();
     const file: File | null = data.get("file") as unknown as File;
     const uploadData = await pinata.upload.file(file);
