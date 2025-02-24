346c864de3908d5113f318dca3a8ef162e90adcc
Norman <norman@samourai.coop>
2025-02-24 16:31:08 +0100

    chore: missing file
    Signed-off-by: Norman <norman@samourai.coop>

---
+++ lib/web3-img-loader.ts
@@ -0,0 +1,13 @@
+"use client";
+
+import { ImageLoaderProps } from "next/image";
+import { web2URL } from "./uris";
+
+// NOTE: be careful, this will serve unoptimized images for web2 srcs,
+// this should only be used in web3 images
+export function web3ImgLoader({ src, width }: ImageLoaderProps) {
+  if (src.startsWith("ipfs://")) {
+    return `${web2URL(src)}?img-width=${width}`;
+  }
+  return src;
+}
