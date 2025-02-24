ca91d85ad5a3d147505e5f5b1c01817f00f8d86b
Norman <norman@samourai.coop>
2025-02-24 16:22:59 +0100

    chore: better type safety
    Signed-off-by: Norman <norman@samourai.coop>

--- lib/loader.ts
+++ lib/loader.ts
@@ -1,14 +1,9 @@
 "use client";
 
+import { ImageLoaderProps } from "next/image";
 import { web2URL } from "./uris";
 
-export default function web3Loader({
-  src,
-  width,
-}: {
-  src: string;
-  width: number;
-}) {
+export default function web3Loader({ src, width }: ImageLoaderProps) {
   if (src.startsWith("ipfs://")) {
     return `${web2URL(src)}?img-width=${width}&img-dpr=2`;
   }
