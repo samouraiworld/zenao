2409c8b5c0c1da9a6a3a402c8ba59c81e95a1d2b
n0izn0iz <n0izn0iz@users.noreply.github.com>
2025-02-24 05:41:17 +0100

    feat: event page background image blur (#203)
    * feat: event page background image blur
    
    Signed-off-by: Norman <norman@samourai.coop>
    
    * chore: slightly reduce blur
    
    Signed-off-by: Norman <norman@samourai.coop>
    
    * chore: re-increase blu
    
    Signed-off-by: Norman <norman@samourai.coop>
    
    * chore: don't repeat background horizontally
    
    Signed-off-by: Norman <norman@samourai.coop>
    
    * chore: reduce blur for chrome
    
    Signed-off-by: Norman <norman@samourai.coop>
    
    * chore: reduce opacity
    
    Signed-off-by: Norman <norman@samourai.coop>
    
    * fix: don't shadow buttons
    
    Signed-off-by: Norman <norman@samourai.coop>
    
    ---------
    
    Signed-off-by: Norman <norman@samourai.coop>

--- components/layout/ScreenContainer.tsx
+++ components/layout/ScreenContainer.tsx
@@ -2,18 +2,36 @@ import React from "react";
 import { SignedOutModal } from "../modals/SignedOutModal";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { web2URL } from "@/lib/uris";
 
 interface ScreenContainerProps {
   children: React.ReactNode;
   isSignedOutModal?: boolean;
+  backgroundSource?: string;
 }
 
 export const ScreenContainer: React.FC<ScreenContainerProps> = ({
   children,
   isSignedOutModal,
+  backgroundSource,
 }) => {
+  const backgroundStyle: React.CSSProperties = backgroundSource
+    ? {
+        backgroundImage: `url(${web2URL(backgroundSource)})`,
+        filter: `blur(200px)`,
+        backgroundSize: "100% auto",
+        position: `absolute`,
+        width: "100%",
+        height: "100%",
+        opacity: 0.15,
+        transition: "background-image 2s",
+        zIndex: -1,
+      }
+    : {};
+
   return (
     <div className="h-screen flex flex-col family-name:var(--font-geist-sans)]">
+      {!!backgroundSource && <div style={backgroundStyle}></div>}
       <Header />
       <div className="flex flex-col flex-1 items-center">
         <main className="h-full w-full max-w-[960px] mb-10 sm:mb-0">
