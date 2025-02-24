02244a2cb6678b5b3264b97e17029b3d27135220
Norman <norman@samourai.coop>
2025-02-24 07:02:48 +0100

    fix: add-back priority
    Signed-off-by: Norman <norman@samourai.coop>

--- components/cards/EventCard.tsx
+++ components/cards/EventCard.tsx
@@ -63,6 +63,7 @@ export function EventCard({ evt }: { evt: EventInfo }) {
               <div className="min-w-[80px] min-h-[80px] w-[80px] h-[80px] sm:w-[120px] sm:h-[120px] relative">
                 <Image
                   src={web2URL(evt.imageUri) + "?img-width=240"}
+                  priority
                   fill
                   alt="event-image"
                   className="object-cover rounded-xl"
