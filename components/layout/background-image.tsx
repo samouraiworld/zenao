d89faba83350661243f4535d9b73c9963e4e67f6
Norman <norman@samourai.coop>
2025-02-24 18:03:18 +0100

    chore: use tailwind instead of style object
    Signed-off-by: Norman <norman@samourai.coop>

--- components/layout/background-image.tsx
+++ components/layout/background-image.tsx
@@ -4,24 +4,15 @@ import Image, { ImageLoaderProps } from "next/image";
 import { web3ImgLoader } from "@/lib/web3-img-loader";
 
 export function BackgroundImage(props: ImageLoaderProps) {
-  const style = {
-    filter: `blur(8rem)`,
-    position: `absolute`,
-    width: "100%",
-    height: "100%",
-    zIndex: -1,
-  } as const;
-
   return (
     <Image
-      style={style}
       alt="Background"
       src={props.src}
       width={props.width}
       height={props.width}
       priority
       loader={web3ImgLoader}
-      className="opacity-25 sm:opacity-15"
+      className={`absolute -z-1 w-full h-full blur-[8rem] opacity-25 sm:opacity-15`}
     />
   );
 }
