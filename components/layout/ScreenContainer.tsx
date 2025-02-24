bc8aa332014fe3ab92436bfdcdc4f661a15afe93
Norman <norman@samourai.coop>
2025-02-24 04:56:41 +0100

    feat: event page background image blur
    Signed-off-by: Norman <norman@samourai.coop>

--- components/layout/ScreenContainer.tsx
+++ components/layout/ScreenContainer.tsx
@@ -2,18 +2,34 @@ import React from "react";
 import { SignedOutModal } from "../modals/SignedOutModal";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { web2URL } from "@/lib/uris";
 
 interface ScreenContainerProps {
   children: React.ReactNode;
   isSignedOutModal?: boolean;
+  backgroundSource?: string;
 }
 
 export const ScreenContainer: React.FC<ScreenContainerProps> = ({
   children,
   isSignedOutModal,
+  backgroundSource,
 }) => {
+  const backgroundStyle: React.CSSProperties = backgroundSource
+    ? {
+        backgroundImage: `url(${web2URL(backgroundSource)})`,
+        filter: `blur(200px)`,
+        position: `absolute`,
+        width: "100%",
+        height: "100%",
+        opacity: 0.2,
+        transition: "background-image 2s",
+      }
+    : {};
+
   return (
     <div className="h-screen flex flex-col family-name:var(--font-geist-sans)]">
+      {!!backgroundSource && <div style={backgroundStyle}></div>}
       <Header />
       <div className="flex flex-col flex-1 items-center">
         <main className="h-full w-full max-w-[960px] mb-10 sm:mb-0">
