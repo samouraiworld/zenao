60667ac220ecab33bd721a839525c9a2ea4702a0
clegirar <clemntgirard@gmail.com>
2025-02-24 10:36:16 +0100

    fix: adapt userFromAddress query with profile integration
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- lib/queries/user.ts
+++ lib/queries/user.ts
@@ -27,26 +27,6 @@ export const userFromAddress = (address: string) =>
       if (!process.env.NEXT_PUBLIC_ZENAO_GNO_ENDPOINT) {
         return null;
       }
-      const client = new GnoJSONRPCProvider(
-        process.env.NEXT_PUBLIC_ZENAO_GNO_ENDPOINT,
-      );
-      const resUsername = await client.evaluateExpression(
-        `gno.land/r/demo/profile`,
-        `GetStringField(${JSON.stringify(address)}, DisplayName, "")`,
-      );
-      const resBio = await client.evaluateExpression(
-        `gno.land/r/demo/profile`,
-        `GetStringField(${JSON.stringify(address)}, Bio, "")`,
-      );
-      const resAvatarURI = await client.evaluateExpression(
-        `gno.land/r/demo/profile`,
-        `GetStringField(${JSON.stringify(address)}, Avatar, "")`,
-      );
-
-      return {
-        displayName: extractGnoStringResponse(resUsername),
-        bio: extractGnoStringResponse(resBio),
-        avatarUri: extractGnoStringResponse(resAvatarURI),
-      };
+      return profiles.fetch(address);
     },
   });
