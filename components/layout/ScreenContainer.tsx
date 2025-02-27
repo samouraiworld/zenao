bfab30195553fe76e9412169fd03df0a2a1c7a3c
Norman <norman@samourai.coop>
2025-02-27 07:12:21 +0100

    chore: use a base64 uri
    Signed-off-by: Norman <norman@samourai.coop>

--- components/layout/ScreenContainer.tsx
+++ components/layout/ScreenContainer.tsx
@@ -6,7 +6,6 @@ import { SignedOutModal } from "../modals/SignedOutModal";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { BackgroundImage } from "./background-image";
-import { web2URL } from "@/lib/uris";
 
 interface ScreenContainerProps {
   children: React.ReactNode;
@@ -15,9 +14,7 @@ interface ScreenContainerProps {
 }
 
 const defaultBackground: ImageLoaderProps = {
-  src: web2URL(
-    "ipfs://bafkreiefzfl43okbnnjwbazkntunp6plgpwnxj7rbqbcukpcnwtstuq4bu",
-  ),
+  src: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAMAAAC67D+PAAAA1VBMVEXr7PLu8Pft7vTr7fLt7/Xz8/b8fgDw9v7v8fft8vvq6fLq6/Lp6fH0n4fvQmn5mWf1t139uAD6bwATddosTNZJh8sAKcdlFr+3PrOWAIzVLIDUZ7f9v3B/muflhJPw8fjv9fnw9v/x8vbp6fT////99/f/5drZ3PD/483p5vb07vXs6vXz6/P/8/KxufLe3/G2v/G/wvD64enWyeiTt+i8vuHn2d3Lv92Hide9uNa/Z9DAms//6cNjUsLpxb/3xKH0opjxzJXdTY/PZHX+5GDyX1v7pka0v0S7AAAAJHRSTlPBzcPDrhv839nOx6Wf/Pz8/Pz8+/v7+/v7+/v4+Pf339zMsRdzt4hNAAAAcElEQVQI1xXMRQKDMAAEwA0hQCl1dyNA3Q33/z8JuM5hwLaKLK/Wisgg6twy+OOk10H4XGo1mrM9xcaS8iBMJk8BNaOTup/X4EpBzu3Id37DW6XdLLa93gUgh+V0POovTJTZ8ft/380yYxoEVaXQdgVyBAtks6JvagAAAABJRU5ErkJggg==",
   width: 10,
 };
 
