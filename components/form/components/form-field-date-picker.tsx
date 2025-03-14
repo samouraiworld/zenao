facb713713ebfe7204b7e14dd0a91ef07d7154b3
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-03-14 14:35:16 +0100

    add new  date time picker + update calendar

--- components/form/components/form-field-date-picker.tsx
+++ components/form/components/form-field-date-picker.tsx
@@ -4,8 +4,16 @@ import { FieldValues } from "react-hook-form";
 import React, { useState } from "react";
 import { CalendarIcon } from "lucide-react";
 import { format, fromUnixTime, getUnixTime } from "date-fns";
+import { format as formatTZ } from "date-fns-tz";
+import { Matcher } from "react-day-picker";
 import { FormFieldProps } from "../types";
-import { FormControl, FormField, FormItem } from "@/components/shadcn/form";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/shadcn/form";
 import {
   Popover,
   PopoverContent,
@@ -14,65 +22,140 @@ import {
 import { Button } from "@/components/shadcn/button";
 import { cn } from "@/lib/tailwind";
 import { Calendar } from "@/components/shadcn/calendar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/shadcn/select";
+import { ScrollArea } from "@/components/shadcn/scroll-area";
+import { SmallText } from "@/components/texts/SmallText";
 
 export function FormFieldDatePickerV2<T extends FieldValues>(
-  props: FormFieldProps<T, bigint> & { timeZone: string },
+  props: FormFieldProps<T, bigint> & {
+    timeZone: string;
+    dateRangeMatcher?: Matcher | Matcher[];
+  },
 ) {
   const [isOpen, setIsOpen] = useState(false);
-  // const { field } = useController(props);
 
   return (
-    <FormField
-      control={props.control}
-      name={props.name}
-      render={({ field }) => {
-        const formattedValue = fromUnixTime(Number(field.value));
+    <div className="flex w-full gap-4">
+      <FormField
+        control={props.control}
+        name={props.name}
+        render={({ field }) => {
+          const formattedValue = fromUnixTime(Number(field.value));
 
-        return (
-          <FormItem className="flex flex-col">
-            <Popover open={isOpen} onOpenChange={setIsOpen}>
-              <PopoverTrigger asChild>
-                <FormControl>
-                  <Button
-                    variant={"outline"}
-                    className={cn(
-                      "w-full font-normal",
-                      !field.value && "text-muted-foreground",
-                    )}
-                  >
-                    {field.value ? (
-                      format(formattedValue, "dd mm yyyy")
-                    ) : (
-                      <span>Pick a date</span>
-                    )}
-                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                  </Button>
-                </FormControl>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto max-auto p-0" align="start">
-                <Calendar
-                  mode="single"
-                  captionLayout="dropdown"
-                  selected={formattedValue}
-                  onSelect={(selectedDate) => {
-                    // const [hours, minutes] = time.split(":")!;
-                    // selectedDate?.setHours(parseInt(hours), parseInt(minutes));
-                    if (selectedDate) {
-                      field.onChange(BigInt(getUnixTime(selectedDate)));
-                    }
+          return (
+            <FormItem className="flex flex-col w-full">
+              <Popover open={isOpen} onOpenChange={setIsOpen}>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-full font-normal",
+                        !field.value && "text-muted-foreground",
+                      )}
+                    >
+                      {field.value ? (
+                        props.timeZone.length ? (
+                          formatTZ(formattedValue, "MM/dd/yyyy", {
+                            timeZone: props.timeZone,
+                          })
+                        ) : (
+                          format(formattedValue, "MM/dd/yyyy")
+                        )
+                      ) : (
+                        <SmallText variant="secondary">
+                          {props.placeholder}
+                        </SmallText>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto max-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    captionLayout="dropdown"
+                    selected={formattedValue}
+                    onSelect={(selectedDate) => {
+                      const hours = formattedValue.getHours();
+                      const minutes = formattedValue.getMinutes();
+
+                      selectedDate?.setHours(hours, minutes);
+                      if (selectedDate) {
+                        field.onChange(BigInt(getUnixTime(selectedDate)));
+                      }
+                    }}
+                    onDayClick={() => setIsOpen(false)}
+                    disabled={props.dateRangeMatcher}
+                    defaultMonth={formattedValue}
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>Set your date and time.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          );
+        }}
+      />
+      <FormField
+        control={props.control}
+        name={props.name}
+        render={({ field }) => {
+          const formattedValue = fromUnixTime(Number(field.value));
+
+          const hour = formattedValue.getHours().toString().padStart(2, "0");
+          const minutes = formattedValue
+            .getMinutes()
+            .toString()
+            .padStart(2, "0");
+          const time = `${hour}:${minutes}`;
+
+          return (
+            <FormItem className="flex flex-col">
+              <FormControl>
+                <Select
+                  defaultValue={time}
+                  onValueChange={(e) => {
+                    const [hours, minutes] = e.split(":");
+
+                    const newDate = new Date(formattedValue);
+
+                    newDate.setHours(parseInt(hours), parseInt(minutes));
+                    field.onChange(BigInt(getUnixTime(newDate)));
                   }}
-                  onDayClick={() => setIsOpen(false)}
-                  // disabled={(date) =>
-                  //   Number(date) < Date.now() - 1000 * 60 * 60 * 24 ||
-                  //   Number(date) > Date.now() + 1000 * 60 * 60 * 24 * 30
-                  // }
-                  defaultMonth={formattedValue}
-                />
-              </PopoverContent>
-            </Popover>
-          </FormItem>
-        );
-      }}
-    />
+                >
+                  <SelectTrigger className="font-normal focus:ring-0 w-[120px] focus:ring-offset-0">
+                    <SelectValue />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <ScrollArea className="h-[15rem]">
+                      {Array.from({ length: 96 }).map((_, i) => {
+                        const hour = Math.floor(i / 4)
+                          .toString()
+                          .padStart(2, "0");
+                        const minute = ((i % 4) * 15)
+                          .toString()
+                          .padStart(2, "0");
+                        return (
+                          <SelectItem key={i} value={`${hour}:${minute}`}>
+                            {hour}:{minute}
+                          </SelectItem>
+                        );
+                      })}
+                    </ScrollArea>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+            </FormItem>
+          );
+        }}
+      />
+    </div>
   );
 }
