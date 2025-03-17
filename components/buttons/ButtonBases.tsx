1d4361d243cf1385e195f11661cdbdb5ac158c97
WaDadidou <wadadidou@gmail.com>
2025-03-16 20:50:56 -0400

    fix: Use ShadCnButtonProps, add type button

--- components/buttons/ButtonBases.tsx
+++ components/buttons/ButtonBases.tsx
@@ -2,15 +2,21 @@
 
 import { Loader2 } from "lucide-react";
 import { Button } from "../shadcn/button";
+import { ButtonProps } from "@/components/buttons/types";
 
 export const ButtonBase: React.FC<
-  React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  ButtonProps & {
     children: React.ReactNode;
     loading?: boolean;
   }
 > = ({ children, loading, disabled, ...otherProps }) => {
   return (
-    <Button className="w-full" disabled={disabled || loading} {...otherProps}>
+    <Button
+      type="button"
+      className="w-full"
+      disabled={disabled || loading}
+      {...otherProps}
+    >
       <div className={loading ? "opacity-0" : undefined}>{children}</div>
       {!!loading && <Loader2 className="animate-spin absolute" />}
     </Button>
