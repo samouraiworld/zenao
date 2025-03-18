5d8c0f808ba1cf2513a944fb911fe95270b6221d
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-03-18 11:19:04 +0100

    test: fix assertion event created

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -237,8 +237,8 @@ describe("main", () => {
         "Join Alice Tester for a fun and interactive event where developers, QA engineers, and tech enthusiasts come together to squash bugs, test workflows, and celebrate the art of quality assurance!",
       )
       .should("be.visible"); // desc
-    cy.get("p").contains("/13/").should("be.visible"); // start date
-    cy.get("p").contains("/14/").should("be.visible"); // end date
+    cy.get("p").contains(" 13th, ").should("be.visible"); // start date
+    cy.get("p").contains(" 14, ").should("be.visible"); // end date
 
     cy.get("button").contains("Participate").should("be.visible");
 
