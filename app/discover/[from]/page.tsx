58b34ad709da23ae5756e54179a4906108c0b950
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-04-03 14:08:12 +0200

    use workaround /discover/[from] to use isr

--- app/discover/[from]/page.tsx
+++ app/discover/[from]/page.tsx
@@ -1,40 +1,38 @@
 import { getTranslations } from "next-intl/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { SearchParams } from "nuqs";
-import { DiscoverEventsList } from "./discover-events-list";
+import { redirect, RedirectType } from "next/navigation";
+import { DiscoverEventsList } from "../discover-events-list";
 import { getQueryClient } from "@/lib/get-query-client";
 import { ScreenContainer } from "@/components/layout/ScreenContainer";
 import { eventsList } from "@/lib/queries/events-list";
 import { EventsListLayout } from "@/components/layout/events-list-layout";
-import { loadEventFilterSearchParams } from "@/lib/searchParams";
 
 export const revalidate = 60;
-export const dynamicParams = true;
-export const dynamic = "force-static";
 
 export async function generateStaticParams() {
   return [];
 }
 
 type PageProps = {
-  searchParams: Promise<SearchParams>;
+  params: Promise<{ from: string }>;
 };
 
-export default async function DiscoverPage({ searchParams }: PageProps) {
+export default async function DiscoverPage({ params }: PageProps) {
+  const { from } = await params;
+
   const queryClient = getQueryClient();
   const now = Date.now() / 1000;
 
-  const { from } = await loadEventFilterSearchParams(searchParams);
+  if (from !== "upcoming" && from !== "past") {
+    redirect("/discover", RedirectType.replace);
+  }
 
-  const upcoming = await queryClient.fetchQuery(
-    eventsList(now, Number.MAX_SAFE_INTEGER, 20, {
-      staleTime: 60000,
-    }),
-  );
-  const past = await queryClient.fetchQuery(
-    eventsList(now - 1, 0, 20, {
-      staleTime: 60000,
-    }),
+  queryClient.prefetchQuery(
+    from === "upcoming"
+      ? eventsList(now, Number.MAX_SAFE_INTEGER, 20)
+      : eventsList(now - 1, 0, 20, {
+          staleTime: 60000,
+        }),
   );
 
   const t = await getTranslations("discover");
@@ -43,7 +41,7 @@ export default async function DiscoverPage({ searchParams }: PageProps) {
     <HydrationBoundary state={dehydrate(queryClient)}>
       <ScreenContainer>
         <EventsListLayout title={t("title")} description={t("description")}>
-          <DiscoverEventsList from={from} upcoming={upcoming} past={past} />
+          <DiscoverEventsList from={from} now={now} />
         </EventsListLayout>
       </ScreenContainer>
     </HydrationBoundary>
--- app/discover/discover-events-list.tsx
+++ app/discover/discover-events-list.tsx
@@ -1,14 +1,23 @@
-import { EventInfo } from "@/app/gen/zenao/v1/zenao_pb";
+"use client";
+
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { EventsList } from "@/components/lists/events-list";
+import { eventsList } from "@/lib/queries/events-list";
 
 export function DiscoverEventsList({
   from,
-  upcoming,
-  past,
+  now,
 }: {
   from: "upcoming" | "past";
-  upcoming: EventInfo[];
-  past: EventInfo[];
+  now: number;
 }) {
-  return <EventsList list={from === "upcoming" ? upcoming : past} />;
+  const { data: events } = useSuspenseQuery(
+    from === "upcoming"
+      ? eventsList(now, Number.MAX_SAFE_INTEGER, 20)
+      : eventsList(now - 1, 0, 20, {
+          staleTime: 60000,
+        }),
+  );
+
+  return <EventsList list={events} />;
 }
