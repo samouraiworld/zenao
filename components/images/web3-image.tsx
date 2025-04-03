8245cbd16cc6565366d4858f6d9646d4aca6eaa8
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-04-03 19:32:29 +0200

    web3image: omit loader

--- components/images/web3-image.tsx
+++ components/images/web3-image.tsx
@@ -4,20 +4,21 @@ import React from "react";
 import Image, { ImageProps } from "next/image";
 import { web3ImgLoader } from "@/lib/web3-img-loader";
 
-export const Web3Image = React.forwardRef<HTMLImageElement, ImageProps>(
-  ({ alt, src, ...props }, ref) => {
-    const isWeb3 = typeof src === "string" && src.startsWith("ipfs://");
+export const Web3Image = React.forwardRef<
+  HTMLImageElement,
+  Omit<ImageProps, "loader">
+>(({ alt, src, ...props }, ref) => {
+  const isWeb3 = typeof src === "string" && src.startsWith("ipfs://");
 
-    return (
-      <Image
-        ref={ref}
-        src={src}
-        alt={alt}
-        loader={isWeb3 ? web3ImgLoader : undefined}
-        {...props}
-      />
-    );
-  },
-);
+  return (
+    <Image
+      ref={ref}
+      src={src}
+      alt={alt}
+      loader={isWeb3 ? web3ImgLoader : undefined}
+      {...props}
+    />
+  );
+});
 
 Web3Image.displayName = "Web3Image";
