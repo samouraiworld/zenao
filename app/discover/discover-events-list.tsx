28c56e242c5588dbf2191fbce84b6747b7f93222
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-04-03 10:25:57 +0200

    fix server side rendering for discover without js enabled

--- app/discover/discover-events-list.tsx
+++ app/discover/discover-events-list.tsx
@@ -1,23 +1,26 @@
-"use client";
-
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { parseAsStringLiteral, useQueryState } from "nuqs";
-import { eventsList } from "@/lib/queries/events-list";
+// import { useSuspenseQuery } from "@tanstack/react-query";
+// import { parseAsStringLiteral, useQueryState } from "nuqs";
+import { EventInfo } from "@/app/gen/zenao/v1/zenao_pb";
+// import { eventsList } from "@/lib/queries/events-list";
 import { EventsList } from "@/components/lists/events-list";
 
-export function DiscoverEventsList({ now }: { now: number }) {
-  const [from] = useQueryState<"upcoming" | "past">(
-    "from",
-    parseAsStringLiteral(["upcoming", "past"] as const)
-      .withDefault("upcoming")
-      .withOptions({ shallow: false, throttleMs: 200 }),
-  );
+export function DiscoverEventsList({
+  from,
+  upcoming,
+  past,
+}: {
+  from: "upcoming" | "past";
+  upcoming: EventInfo[];
+  past: EventInfo[];
+}) {
+  // const [from] = useQueryState<"upcoming" | "past">(
+  //   "from",
+  //   parseAsStringLiteral(["upcoming", "past"] as const)
+  //     .withDefault("upcoming")
+  //     .withOptions({ shallow: false, throttleMs: 200 }),
+  // );
 
-  const { data: events } = useSuspenseQuery(
-    from === "upcoming"
-      ? eventsList(now, Number.MAX_SAFE_INTEGER, 20)
-      : eventsList(now - 1, 0, 20),
-  );
+  console.log(from);
 
-  return <EventsList list={events} />;
+  return <EventsList list={from === "upcoming" ? upcoming : past} />;
 }
