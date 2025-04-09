36a7ccc587fb4a725c931efc78b19a3e00194979
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-04-09 17:45:08 +0200

    add parsing multiaddr poll uri

--- lib/multiaddr.ts
+++ lib/multiaddr.ts
@@ -1,21 +1,44 @@
-import {
-  multiaddr,
-  resolvers,
-  createProtocol,
-  registerProtocol,
-} from "@multiformats/multiaddr";
-import { PostView } from "@/app/gen/feeds/v1/feeds_pb";
+import { multiaddr, registerProtocol } from "@multiformats/multiaddr";
 
-export const tryOut = (posts: PostView[]) => {
-  const polls = posts.filter((post) => {
-    return post.post?.post.case === "link" && post.post?.tags?.includes("poll");
-  });
+registerProtocol({
+  name: "poll",
+  code: 0x301,
+  size: 64,
+});
 
-  // registerProtocol();
-  console.log("Polls: ", polls);
+registerProtocol({
+  name: "gno",
+  code: 0x300,
+  size: -1,
+  path: true,
+  resolvable: false,
+});
 
-  // muut
+export const parsePollUri = (uri: string) => {
+  const parsed = multiaddr(uri);
+  const protoNames = parsed.protoNames();
 
-  // Multiaddr to fetch poll id
-  // const
+  if (!protoNames.includes("gno") || !protoNames.includes("poll")) {
+    throw new Error("Invalid URI: must contain both gno and poll protocols");
+  }
+
+  const gnoPath = parsed.getPath()?.slice(1);
+  const pollId = new TextDecoder().decode(parsed.tuples()[0][1]);
+
+  return { gnoPath, pollId };
 };
+
+// export const tryOut = (posts: PostView[]) => {
+//   const polls = posts.filter((post) => {
+//     return post.post?.post.case === "link" && post.post?.tags?.includes("poll");
+//   });
+
+//   parsePollUri(example);
+
+//   // console.log(fromString("gnolandr/zenao/polls", "base16"));
+
+//   // muut
+
+//   // Multiaddr to fetch poll id
+//   // const
+// };
