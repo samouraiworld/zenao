617ad9e3455b10336bdc66b1b5e2ba56e9ddf96c
MikaelVallenet <mikael.vallenetpro@gmail.com>
2025-04-10 16:12:01 +0200

    chore: regenerate sql schema

--- migrations/20250410141140_feeds.sql
+++ migrations/20250410141140_feeds.sql
@@ -3,7 +3,7 @@ CREATE TABLE `feeds` (`id` integer NULL PRIMARY KEY AUTOINCREMENT, `created_at` 
 -- Create index "idx_feeds_deleted_at" to table: "feeds"
 CREATE INDEX `idx_feeds_deleted_at` ON `feeds` (`deleted_at`);
 -- Create "posts" table
-CREATE TABLE `posts` (`id` integer NULL PRIMARY KEY AUTOINCREMENT, `created_at` datetime NULL, `updated_at` datetime NULL, `deleted_at` datetime NULL, `kind` text NULL, `parent_uri` text NULL, `latitude` real NULL, `longitude` real NULL, `tags` text NULL, `content` text NULL, `title` text NULL, `preview_text` text NULL, `preview_image_uri` text NULL, `uri` text NULL, `description` text NULL, `image_uri` text NULL, `audio_uri` text NULL, `video_uri` text NULL, `thumbnail_image_uri` text NULL, `user_id` integer NULL, `feed_id` integer NULL, CONSTRAINT `fk_posts_feed` FOREIGN KEY (`feed_id`) REFERENCES `feeds` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION, CONSTRAINT `fk_posts_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION);
+CREATE TABLE `posts` (`id` integer NULL PRIMARY KEY AUTOINCREMENT, `created_at` datetime NULL, `updated_at` datetime NULL, `deleted_at` datetime NULL, `kind` text NULL, `parent_uri` text NULL, `latitude` real NULL, `longitude` real NULL, `content` text NULL, `title` text NULL, `preview_text` text NULL, `preview_image_uri` text NULL, `uri` text NULL, `description` text NULL, `image_uri` text NULL, `audio_uri` text NULL, `video_uri` text NULL, `thumbnail_image_uri` text NULL, `user_id` integer NULL, `feed_id` integer NULL, CONSTRAINT `fk_posts_feed` FOREIGN KEY (`feed_id`) REFERENCES `feeds` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION, CONSTRAINT `fk_posts_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION);
 -- Create index "idx_posts_deleted_at" to table: "posts"
 CREATE INDEX `idx_posts_deleted_at` ON `posts` (`deleted_at`);
 -- Create "polls" table
@@ -16,3 +16,5 @@ CREATE TABLE `poll_results` (`id` integer NULL PRIMARY KEY AUTOINCREMENT, `creat
 CREATE INDEX `idx_poll_results_deleted_at` ON `poll_results` (`deleted_at`);
 -- Create "poll_votes" table
 CREATE TABLE `poll_votes` (`poll_result_id` integer NULL, `user_id` integer NULL, PRIMARY KEY (`poll_result_id`, `user_id`), CONSTRAINT `fk_poll_votes_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION, CONSTRAINT `fk_poll_votes_poll_result` FOREIGN KEY (`poll_result_id`) REFERENCES `poll_results` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION);
+-- Create "tags" table
+CREATE TABLE `tags` (`post_id` integer NULL, `name` text NULL, PRIMARY KEY (`post_id`, `name`), CONSTRAINT `fk_posts_tags` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION);
