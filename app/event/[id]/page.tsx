20c642baa17c7fe680fdd649580b29edb61d6cbf
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-04-22 08:31:31 +0200

    refacto try catch

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -26,16 +26,17 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const id = (await params).id;
 
   const queryClient = getQueryClient();
-  const event = await queryClient.fetchQuery(eventOptions(id)).catch((_) => {
+  try {
+    const event = await queryClient.fetchQuery(eventOptions(id));
+    return {
+      title: event.title,
+      openGraph: {
+        images: [{ url: web2URL(event.imageUri) }],
+      },
+    };
+  } catch (_) {
     notFound();
-  });
-
-  return {
-    title: event.title,
-    openGraph: {
-      images: [{ url: web2URL(event.imageUri) }],
-    },
-  };
+  }
 }
 
 export default async function EventPage({ params }: Props) {
@@ -44,36 +45,35 @@ export default async function EventPage({ params }: Props) {
   const p = await params;
   const queryClient = getQueryClient();
 
-  const eventData = await queryClient
-    .fetchQuery({
+  try {
+    const eventData = await queryClient.fetchQuery({
       ...eventOptions(p.id),
-    })
-    .catch((err) => {
-      console.error("error", err);
-      notFound();
     });
+    queryClient.prefetchQuery(profileOptions(eventData.creator));
 
-  queryClient.prefetchQuery(profileOptions(eventData.creator));
+    // Prefetch all participants profiles
+    const addresses = await queryClient.fetchQuery(
+      eventUsersWithRole(p.id, "participant"),
+    );
+    addresses.forEach(
+      (address) => void queryClient.prefetchQuery(profileOptions(address)),
+    );
 
-  // Prefetch all participants profiles
-  const addresses = await queryClient.fetchQuery(
-    eventUsersWithRole(p.id, "participant"),
-  );
-  addresses.forEach(
-    (address) => void queryClient.prefetchQuery(profileOptions(address)),
-  );
-
-  return (
-    <ScreenContainer
-      background={{
-        src: eventData.imageUri,
-        width: imageWidth,
-        height: imageHeight,
-      }}
-    >
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <EventInfo id={p.id} />
-      </HydrationBoundary>
-    </ScreenContainer>
-  );
+    return (
+      <ScreenContainer
+        background={{
+          src: eventData.imageUri,
+          width: imageWidth,
+          height: imageHeight,
+        }}
+      >
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <EventInfo id={p.id} />
+        </HydrationBoundary>
+      </ScreenContainer>
+    );
+  } catch (err) {
+    console.error("error", err);
+    notFound();
+  }
 }
