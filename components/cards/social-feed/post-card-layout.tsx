daf9efb720ee12d147fde0a7bcc82edcf66d0131
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-04-28 12:04:43 +0200

    correct handler on click existing reaction

--- components/cards/social-feed/post-card-layout.tsx
+++ components/cards/social-feed/post-card-layout.tsx
@@ -170,12 +170,13 @@ function Reactions({
       {reactions.map((reaction) => (
         <div
           onClick={() => {
-            if (reaction.userHasVoted) onReactionChange(reaction.icon);
+            if (isOrganizer || isParticipant) onReactionChange(reaction.icon);
           }}
           className={cn(
             "flex flex-row items-center py-0.5 pl-1 pr-2 rounded-full gap-0.5 cursor-default",
-            reaction.userHasVoted &&
-              "border-[1px] border-neutral-700 hover:bg-neutral-700 cursor-pointer",
+            reaction.userHasVoted && "border-[1px] border-neutral-700",
+            (isOrganizer || isParticipant) &&
+              "hover:bg-neutral-700 cursor-pointer",
             !reaction.userHasVoted && "bg-neutral-800",
           )}
           key={reaction.icon}
