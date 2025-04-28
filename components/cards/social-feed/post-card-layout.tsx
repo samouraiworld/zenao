f8324ed0871676f787bb7ec3f61df053e19dadee
WaDadidou <wadadidou@gmail.com>
2025-04-28 14:09:27 +0200

    fix(feed): Sort Reactions by count

--- components/cards/social-feed/post-card-layout.tsx
+++ components/cards/social-feed/post-card-layout.tsx
@@ -167,25 +167,27 @@ function Reactions({
         </Popover>
       )}
 
-      {reactions.map((reaction) => (
-        <div
-          onClick={() => {
-            if (reaction.userHasVoted) onReactionChange(reaction.icon);
-          }}
-          className={cn(
-            "flex flex-row items-center py-0.5 pl-1 pr-2 rounded-full gap-0.5 cursor-default",
-            reaction.userHasVoted &&
-              "border-[1px] border-neutral-700 hover:bg-neutral-700 cursor-pointer",
-            !reaction.userHasVoted && "bg-neutral-800",
-          )}
-          key={reaction.icon}
-        >
-          <Text className="text-sm">{reaction.icon}</Text>
-          <Text variant="secondary" className="text-sm">
-            {reaction.count}
-          </Text>
-        </div>
-      ))}
+      {reactions
+        .sort((a, b) => b.count - a.count)
+        .map((reaction) => (
+          <div
+            onClick={() => {
+              if (reaction.userHasVoted) onReactionChange(reaction.icon);
+            }}
+            className={cn(
+              "flex flex-row items-center py-0.5 pl-1 pr-2 rounded-full gap-0.5 cursor-default",
+              reaction.userHasVoted &&
+                "border-[1px] border-neutral-700 hover:bg-neutral-700 cursor-pointer",
+              !reaction.userHasVoted && "bg-neutral-800",
+            )}
+            key={reaction.icon}
+          >
+            <Text className="text-sm">{reaction.icon}</Text>
+            <Text variant="secondary" className="text-sm">
+              {reaction.count}
+            </Text>
+          </div>
+        ))}
     </div>
   );
 }
