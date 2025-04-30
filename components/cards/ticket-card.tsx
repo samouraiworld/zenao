1ffc7283d35db6f3587ee6eedd89f0d3196187e1
Thomas MICHEL <thomas1.michel@epitech.eu>
2025-04-30 09:26:52 +0200

    Update components/cards/ticket-card.tsx
    Co-authored-by: ๖ۣۜDadidou <50441633+WaDadidou@users.noreply.github.com>

--- components/cards/ticket-card.tsx
+++ components/cards/ticket-card.tsx
@@ -31,8 +31,8 @@ export function TicketCard({
 
   const x = useMotionValue(0);
   const y = useMotionValue(0);
-  const rotateX = useTransform(x, [-100, 100], [25, -25]);
-  const rotateY = useTransform(y, [-100, 100], [25, -25]);
+  const rotateX = useTransform(y, [-80, 80], [80, -80])
+  const rotateY = useTransform(x, [-80, 80], [-80, 80])
 
   return (
     <div className="card-3d">
