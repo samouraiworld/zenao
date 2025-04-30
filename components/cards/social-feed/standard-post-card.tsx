c92156141fcf8d55a438518ce94dc9e5953ff95a
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-04-30 11:59:55 +0200

    add link gnoweb standard post

--- components/cards/social-feed/standard-post-card.tsx
+++ components/cards/social-feed/standard-post-card.tsx
@@ -5,6 +5,7 @@ import { PostCardLayout } from "@/components/cards/social-feed/post-card-layout"
 import { profileOptions } from "@/lib/queries/profile";
 import { StandardPostView } from "@/lib/social-feed";
 import { MarkdownPreview } from "@/components/common/markdown-preview";
+import { derivePkgAddr } from "@/lib/gno";
 
 export function StandardPostCard({
   eventId,
@@ -18,13 +19,14 @@ export function StandardPostCard({
   );
 
   const standardPost = post.post.post.value;
+  const feedSlug = `${derivePkgAddr(`gno.land/r/zenao/events/e${eventId}`)}:main`;
 
   return (
     <PostCardLayout
       eventId={eventId}
       post={post}
       createdBy={createdBy}
-      gnowebHref={`${process.env.NEXT_PUBLIC_GNOWEB_URL}/r/${process.env.NEXT_PUBLIC_ZENAO_NAMESPACE}`}
+      gnowebHref={`${process.env.NEXT_PUBLIC_GNOWEB_URL}/r/${process.env.NEXT_PUBLIC_ZENAO_NAMESPACE}/social_feed:${feedSlug}`}
     >
       <MarkdownPreview markdownString={standardPost.content} />
     </PostCardLayout>
