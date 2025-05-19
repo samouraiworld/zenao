c37fc83e809517df794554e6ff8e95291f7f4a13
Norman <norman@samourai.coop>
2025-05-19 20:49:13 +0200

    feat: better image optimization params
    Signed-off-by: Norman <norman@samourai.coop>

--- lib/web3-img-loader.ts
+++ lib/web3-img-loader.ts
@@ -3,9 +3,9 @@ import { web2URL } from "./uris";
 
 // NOTE: be careful, this will serve unoptimized images for web2 srcs,
 // this should only be used in web3 images
-export function web3ImgLoader({ src, width }: ImageLoaderProps) {
+export function web3ImgLoader({ src, width, quality }: ImageLoaderProps) {
   if (src.startsWith("ipfs://")) {
-    return `${web2URL(src)}?img-width=${width}`;
+    return `${web2URL(src)}?img-width=${width}&img-quality=${quality}&format=webp`;
   }
   return src;
 }
