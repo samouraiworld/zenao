e8b88e732abb31b4131b9bb3f796dcb38d9a257f
Norman <norman@samourai.coop>
2025-05-19 20:57:05 +0200

    chore: make code more readable
    Signed-off-by: Norman <norman@samourai.coop>

--- lib/web3-img-loader.ts
+++ lib/web3-img-loader.ts
@@ -4,12 +4,13 @@ import { web2URL } from "./uris";
 // NOTE: be careful, this will serve unoptimized images for web2 srcs,
 // this should only be used in web3 images
 export function web3ImgLoader({ src, width, quality }: ImageLoaderProps) {
-  if (src.startsWith("ipfs://")) {
-    let url = `${web2URL(src)}?img-width=${width}&img-format=webp`;
-    if (quality !== undefined) {
-      url += `&img-quality=${quality}`;
-    }
-    return url;
+  if (!src.startsWith("ipfs://")) {
+    return src;
   }
-  return src;
+
+  let url = `${web2URL(src)}?img-width=${width}&img-format=webp`;
+  if (quality !== undefined) {
+    url += `&img-quality=${quality}`;
+  }
+  return url;
 }
