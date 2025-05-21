6604a6e5659a7aa4c7013e4e51e0ad69823c4f30
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-05-21 11:47:43 +0200

    pwa: add manifest + icons + splash

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Zenao",
+    short_name: "Zenao",
+    description: "Organize event(s) in seconds",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#EC7E17",
+    icons: [
+      {
+        src: "/icons/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icons/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
