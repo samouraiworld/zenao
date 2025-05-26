7983c920c4aa5b149ca83af853e3907ad4e3e741
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-05-26 12:33:03 +0200

    one level tabs + fix e2e

--- app/event/[id]/main-event-sections.tsx
+++ app/event/[id]/main-event-sections.tsx
@@ -2,12 +2,17 @@
 
 import { Tabs, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
 import { useTranslations } from "next-intl";
-import { useEffect, useState } from "react";
-import { EventFeed } from "./event-feed";
+import { useEffect, useRef, useState } from "react";
+import { useAuth } from "@clerk/nextjs";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { EventFeedForm } from "./event-feed-form";
 import { Separator } from "@/components/shadcn/separator";
 import { cn } from "@/lib/tailwind";
 import { TabsContent } from "@/components/shadcn/tabs";
 import { MarkdownPreview } from "@/components/common/markdown-preview";
+import { PollsList } from "@/components/widgets/polls-list";
+import { PostsList } from "@/components/widgets/posts-list";
+import { userAddressOptions } from "@/lib/queries/user";
 
 export function MainEventSections({
   className,
@@ -20,8 +25,17 @@ export function MainEventSections({
   isMember: boolean;
   className?: string;
 }) {
-  const [tab, setTab] = useState<"feed" | "description">("feed");
+  const [tab, setTab] = useState<"description" | "feed" | "votes">(
+    "description",
+  );
   const t = useTranslations("event");
+
+  const { getToken, userId } = useAuth();
+  const { data: userAddress } = useSuspenseQuery(
+    userAddressOptions(getToken, userId),
+  );
+
+  const feedFormRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     setTab(isMember ? "feed" : "description");
@@ -45,17 +59,36 @@ export function MainEventSections({
           value="feed"
           className="w-fit p-2 data-[state=active]:font-semibold hover:bg-secondary/80"
         >
-          {t("social-feed")}
+          {t("discussion")}
+        </TabsTrigger>
+        <TabsTrigger
+          value="votes"
+          className="w-fit p-2 data-[state=active]:font-semibold hover:bg-secondary/80"
+        >
+          {t("votes")}
         </TabsTrigger>
       </TabsList>
       <Separator className="mb-3" />
+
       <TabsContent value="description">
         <MarkdownPreview markdownString={description} />
       </TabsContent>
-      {/* Social Feed */}
-      <TabsContent value="feed">
-        <EventFeed eventId={eventId} isMember={isMember} />
-      </TabsContent>
+
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-6 min-h-0 pt-4">
+          {tab !== "description" && isMember && (
+            <EventFeedForm ref={feedFormRef} eventId={eventId} />
+          )}
+          {/* Social Feed (Discussions) */}
+          <TabsContent value="feed">
+            <PostsList eventId={eventId} userAddress={userAddress} />
+          </TabsContent>
+          {/* Social Feed (Votes) */}
+          <TabsContent value="votes">
+            <PollsList eventId={eventId} userAddress={userAddress} />
+          </TabsContent>
+        </div>
+      </div>
     </Tabs>
   );
 }
