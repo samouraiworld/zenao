0f2151a5ebf769a4ad31d6b462b0df450a056173
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-05-29 11:14:18 +0200

    fix freezer drawer on PWA

--- app/event/[id]/participants-section.tsx
+++ app/event/[id]/participants-section.tsx
@@ -86,6 +86,7 @@ export function ParticipantsSection({ id }: { id: string }) {
   );
   const t = useTranslations("event");
   const isDesktop = useMediaQuery("(min-width: 768px)");
+  const isStandalone = useMediaQuery("(display-mode: standalone)");
 
   const onOpenChange = (state: boolean) => {
     setOpen(state);
@@ -131,7 +132,7 @@ export function ParticipantsSection({ id }: { id: string }) {
   }
 
   return (
-    <Drawer open={open} onOpenChange={onOpenChange}>
+    <Drawer open={open} onOpenChange={onOpenChange} modal={!isStandalone}>
       <DrawerTrigger asChild>
         <div className="flex flex-col gap-2">
           {/* 6 because we decide to show the first 6 participants avatars as preview */}
