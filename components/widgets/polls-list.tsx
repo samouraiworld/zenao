79210fe7a58390a4c3eecf423ed9569166500502
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-05-29 11:26:07 +0200

    fix padding no more post label

--- components/widgets/polls-list.tsx
+++ components/widgets/polls-list.tsx
@@ -77,14 +77,16 @@ export function PollsList({
           })
         )}
       </div>
-      <LoaderMoreButton
-        fetchNextPage={fetchNextPage}
-        hasNextPage={hasNextPage}
-        isFetching={isFetching}
-        isFetchingNextPage={isFetchingNextPage}
-        page={polls}
-        noMoreLabel={t("no-more-posts")}
-      />
+      <div className="py-4">
+        <LoaderMoreButton
+          fetchNextPage={fetchNextPage}
+          hasNextPage={hasNextPage}
+          isFetching={isFetching}
+          isFetchingNextPage={isFetchingNextPage}
+          page={polls}
+          noMoreLabel={t("no-more-posts")}
+        />
+      </div>
     </>
   );
 }
