f1e3751eb41803a0b1232e31785d1d03d061a77d
miosan <miosan_desu@proton.me>
2025-06-02 14:38:32 +0200

    Revert "remove reply alert"
    This reverts commit 2aced8a54a3cc848c2d38efecfaf2aa8e088e7b2.

--- components/form/social-feed/standard-post-form.tsx
+++ components/form/social-feed/standard-post-form.tsx
@@ -35,6 +35,7 @@ import {
 import { Textarea } from "@/components/shadcn/textarea";
 import Text from "@/components/texts/text";
 import { uploadFile } from "@/lib/files";
+import { ReplyAlert } from "@/components/widgets/reply-alert";
 import { useCreateStandardPost } from "@/lib/mutations/social-feed";
 import { userAddressOptions } from "@/lib/queries/user";
 import { cn } from "@/lib/tailwind";
@@ -190,6 +191,7 @@ export function StandardPostForm({
         onSubmit={form.handleSubmit(onSubmitStandardPost)}
         className="flex flex-col gap-4 p-4 rounded"
       >
+        <ReplyAlert parentPost={parentPost} form={form} />
         <div className="flex flex-row gap-4">
           <Tabs defaultValue="form" className="w-full">
             <div className="w-full flex justify-between">
