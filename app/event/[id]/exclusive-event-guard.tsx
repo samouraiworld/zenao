16c3522c09563cf6226a62c265ca43629b1b0b88
Mio-san <thomas1.michel@epitech.eu>
2025-06-10 23:43:53 +0200

    fix(front): Update Aspect-Ratio Video Markdown + Disable sentry report for specific errors (#533)
    fix #526
    fix #530
    fix #531
    
    ---------
    
    Co-authored-by: n0izn0iz <n0izn0iz@users.noreply.github.com>

--- app/event/[id]/exclusive-event-guard.tsx
+++ app/event/[id]/exclusive-event-guard.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
 import { useEffect, useMemo, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Loader2, Lock } from "lucide-react";
@@ -26,6 +25,7 @@ import { zenaoClient } from "@/app/zenao-client";
 import { ScreenContainer } from "@/components/layout/screen-container";
 import { AspectRatio } from "@/components/shadcn/aspect-ratio";
 import { Web3Image } from "@/components/images/web3-image";
+import { captureException } from "@/lib/report";
 
 type ExclusiveEventGuardProps = {
   eventId: string;
@@ -85,10 +85,10 @@ export function ExclusiveEventGuard({
       }
       setCanAccess(true);
     } catch (error: unknown) {
-      console.error("Error accessing exclusive event:", error);
       if (error instanceof Error && error.message !== "Invalid password") {
-        Sentry.captureException(error);
+        captureException(error);
       }
+
       toast({
         duration: 3000,
         variant: "destructive",
