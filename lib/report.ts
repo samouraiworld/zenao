ac25489f8256c5409d208385d95bcf4c0458af44
miosan <miosan_desu@proton.me>
2025-06-10 17:35:41 +0200

    sentry: grouping by error message

--- lib/report.ts
+++ lib/report.ts
@@ -3,6 +3,11 @@ import * as Sentry from "@sentry/nextjs";
 export const captureException = (error: unknown) => {
   console.error("Error captured:", error);
   if (process.env.NEXT_PUBLIC_ENV ?? "development" !== "development") {
-    Sentry.captureException(error);
+    Sentry.captureException(error, {
+      fingerprint: [
+        "{{ default }}",
+        error instanceof Error ? error.message : "Unknown error",
+      ],
+    });
   }
 };
