89ce08b55d8ef16d81965342cb29ba9e7d88de22
MikaelVallenet <mikael.vallenetpro@gmail.com>
2025-06-10 17:50:37 +0200

    fix(back): middleware create user on chain only if newly created

--- backend/middlewares.go
+++ backend/middlewares.go
@@ -23,11 +23,13 @@ func (s *ZenaoServer) EnsureUserExists(
 	}
 
 	var zUser *zeni.User
+	var alreadyExists bool
 	if err := s.DB.Tx(func(db zeni.DB) error {
 		var err error
 		if zUser, err = db.GetUser(user.ID); err != nil {
 			return err
 		} else if zUser != nil {
+			alreadyExists = true
 			return nil
 		}
 		if zUser, err = db.CreateUser(user.ID); err != nil {
@@ -38,8 +40,10 @@ func (s *ZenaoServer) EnsureUserExists(
 		return nil, err
 	}
 
-	if err := s.Chain.CreateUser(&zeni.User{ID: zUser.ID}); err != nil {
-		return nil, err
+	if !alreadyExists {
+		if err := s.Chain.CreateUser(&zeni.User{ID: zUser.ID}); err != nil {
+			return nil, err
+		}
 	}
 
 	return zUser, nil
