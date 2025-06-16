2cbd3e5536278515b868a51267d63468ac03e487
Mio-san <thomas1.michel@epitech.eu>
2025-06-16 13:10:34 +0200

    feat(front): PWA Installation dialog + Navigation Bar (#534)
    Fix #522
    
    - Add back button
    - Add bottom bar to feel more native
    - Add dialog pwa installation instructions
    
    <img width="1512" alt="Screenshot 2025-06-10 at 09 44 14"
    src="https://github.com/user-attachments/assets/f1b36bfe-f0ce-4495-9bcf-e8a5a0e3cc64"
    />
    <img width="517" alt="Screenshot 2025-06-10 at 09 44 44"
    src="https://github.com/user-attachments/assets/3fdccb61-4f5f-47e3-9636-061714b7cbd7"
    />
    <img width="362" alt="Screenshot 2025-06-10 at 10 28 25"
    src="https://github.com/user-attachments/assets/43bacdc3-b64d-4aa1-a14a-afebe9d0cf39"
    />
    
    ---------
    
    Co-authored-by: n0izn0iz <n0izn0iz@users.noreply.github.com>
    Co-authored-by: Norman <norman@samourai.coop>

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { useTranslations } from "next-intl";
-import Link from "next/link";
 import Image from "next/image";
 import { ScreenContainerCentered } from "@/components/layout/screen-container";
-import { ButtonWithChildren } from "@/components/buttons/button-with-children";
 import Heading from "@/components/texts/heading";
 import Text from "@/components/texts/text";
-import InstallButton from "@/components/buttons/pwa-install-button";
+import { HomeCTA } from "@/components/widgets/home-cta";
 
 export default function Home() {
   const t = useTranslations("home");
@@ -32,17 +30,7 @@ export default function Home() {
         >
           {t("secondary-text")}
         </Text>
-        <div className="flex flex-col gap-2 items-center">
-          <Link href="/create">
-            <ButtonWithChildren className="w-full flex rounded-3xl py-5">
-              <Text variant="invert" className="text-sm">
-                {t("button")}
-              </Text>
-            </ButtonWithChildren>
-          </Link>
-
-          <InstallButton />
-        </div>
+        <HomeCTA />
       </div>
     </ScreenContainerCentered>
   );
