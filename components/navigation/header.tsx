9c1f53a40e2a1314faacc4acec5d7b679d8b834e
miosan <miosan_desu@proton.me>
2025-06-20 15:54:01 +0200

    fix headerlinks style

--- components/navigation/header.tsx
+++ components/navigation/header.tsx
@@ -183,7 +183,7 @@ export function Header() {
             <Text className="max-md:hidden font-extrabold">{t("zenao")}</Text>
           </Link>
         </div>
-        <div className="hidden standalone:md:flex flex-row gap-4">
+        <div className="flex standalone:hidden standalone:md:flex flex-row gap-4">
           <HeaderLinks />
         </div>
       </div>
