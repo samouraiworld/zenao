6b4082eee766def84d966ecf97963f5b1ab11f17
miosan <miosan_desu@proton.me>
2025-06-24 17:11:15 +0200

    e2e: add wrong ar code scan test

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,23 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(_on, config) {
+    setupNodeEvents(on, config) {
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (browser.family === "chromium" && browser.name !== "electron") {
+          // Mac/Linux
+          launchOptions.args.push(
+            "--use-fake-ui-for-media-stream",
+            "--use-fake-device-for-media-stream",
+            "--use-file-for-fake-video-capture=cypress/fixtures/fake_qr.mjpeg",
+          );
+
+          // Windows
+          // launchOptions.args.push('--use-file-for-fake-video-capture=c:\\path\\to\\video\\my-video.y4m')
+        }
+
+        return launchOptions;
+      });
+
       return clerkSetup({ config });
     },
   },
