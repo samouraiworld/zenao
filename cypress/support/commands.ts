e3f82c74bc23bb860390b04184e1dd612417bf24
miosan <miosan_desu@proton.me>
2025-06-26 17:54:31 +0200

    update command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,19 +1,31 @@
 /// <reference types="cypress" />
 
 import { addClerkCommands } from "@clerk/testing/cypress";
-// import { createSlideshow } from "slideshow-video";
 
 addClerkCommands({ Cypress, cy });
 
 Cypress.Commands.add("generateValidQRVideo", (imgSrc) => {
   const command = `ffmpeg -y -framerate 1/10 -i ${imgSrc} -vf "scale=444:250" -r 30 -pix_fmt yuv420p cypress/fixtures/output.mjpeg`;
-  cy.exec(command).then((result) => {
+  cy.exec(command, { failOnNonZeroExit: false }).then((result) => {
     if (result.code !== 0) {
-      throw new Error("Could not generate error", {
+      throw new Error(result.stderr, {
         cause: result.stderr,
       });
     }
+    cy.log(result.stdout);
+    cy.log(result.stderr);
 
     console.log(`QRCode video saved as: cypress/fixtures/output.mjpeg`);
   });
+});
+
+Cypress.Commands.overwrite("log", function (log, ...args) {
+  if (Cypress.browser.isHeadless) {
+    return cy.task("log", args, { log: false }).then(() => {
+      return log(...args);
+    });
+  } else {
+    console.log(...args);
+    return log(...args);
+  }
 });
