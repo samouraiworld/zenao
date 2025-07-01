e7115404d9f576b73d9cd8a28cf628683121a81e
miosan <miosan_desu@proton.me>
2025-07-01 14:02:05 +0200

    fix lint

--- app/event/[id]/scanner/page.tsx
+++ app/event/[id]/scanner/page.tsx
@@ -1,11 +1,9 @@
 import { notFound, redirect, RedirectType } from "next/navigation";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { auth } from "@clerk/nextjs/server";
-import { imageHeight, imageWidth } from "../constants";
 import { EventTicketScanner } from "./event-ticket-scanner";
 import { getQueryClient } from "@/lib/get-query-client";
 import { eventOptions } from "@/lib/queries/event";
-import { ScreenContainer } from "@/components/layout/screen-container";
 import { userAddressOptions } from "@/lib/queries/user";
 import { eventUserRoles } from "@/lib/queries/event-users";
 
