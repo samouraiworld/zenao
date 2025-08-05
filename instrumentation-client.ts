2c1f89b60be78e542e4d0b99d75509e3a1f04214
Mio-san <thomas1.michel@epitech.eu>
2025-08-05 11:44:46 +0200

    feat(front): add button report bug in footer (#579)
    fix #501 
    
    Button in Footer:
    
    <img width="314" alt="Screenshot 2025-06-20 at 19 48 28"
    src="https://github.com/user-attachments/assets/3bd7beee-e47f-437c-93a5-c5c642f07604"
    />
    
    Bug available in Sentry Dashboard in the section "User Feedback":
    
    <img width="1512" alt="Screenshot 2025-06-20 at 19 47 30"
    src="https://github.com/user-attachments/assets/117cb4d6-e12b-4638-b741-f71c1b63a6d9"
    />
    
    ---------
    
    Co-authored-by: n0izn0iz <n0izn0iz@users.noreply.github.com>

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -13,7 +13,13 @@ Sentry.init({
   tracesSampleRate: 0.7,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: true,
+
+  integrations: [
+    Sentry.feedbackIntegration({
+      autoInject: false,
+    }),
+  ],
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
