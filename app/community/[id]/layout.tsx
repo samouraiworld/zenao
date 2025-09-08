88f8e8678af51e84db9d5418397b8128c6dd52af
moonia <99096342+moonia@users.noreply.github.com>
2025-09-08 15:30:51 +0200

    feat: join community button action (#654)
    https://github.com/samouraiworld/zenao/issues/645
    
    ---------
    
    Co-authored-by: miosan <miosan_desu@proton.me>
    Co-authored-by: n0izn0iz <n0izn0iz@users.noreply.github.com>

--- app/community/[id]/layout.tsx
+++ app/community/[id]/layout.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/lib/queries/community";
 import { profileOptions } from "@/lib/queries/profile";
 import { eventsPkgPathsByAddrs } from "@/lib/queries/events-list";
+import { CommunityJoinButton } from "@/components/community/community-join-button";
 
 // enable ssg for all events
 export async function generateStaticParams() {
@@ -44,6 +45,7 @@ async function CommunityPageLayout({
   const members = await queryClient.fetchQuery(
     communityUsersWithRoles(communityId, ["member"]),
   );
+
   members.forEach((member) =>
     queryClient.prefetchQuery(profileOptions(member.address)),
   );
@@ -69,6 +71,7 @@ async function CommunityPageLayout({
         }}
       >
         <CommunityInfoLayout communityId={communityId}>
+          <CommunityJoinButton communityId={communityId} />
           {children}
         </CommunityInfoLayout>
       </ScreenContainer>
