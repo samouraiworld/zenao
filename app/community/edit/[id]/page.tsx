d2b4b81bad5d43e6d5d8528643ab80d4161aded6
moonia <mounia.arjdal@epitech.eu>
2025-09-13 22:43:34 +0700

    fix: dynamic community_id for edit page

--- app/community/edit/[id]/page.tsx
+++ app/community/edit/[id]/page.tsx
@@ -3,13 +3,18 @@ import { EditCommunityForm } from "./edit-community-form";
 import { getQueryClient } from "@/lib/get-query-client";
 import { ScreenContainer } from "@/components/layout/screen-container";
 
-export default async function CreateEventPage() {
+export default async function EditCommunityPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const queryClient = getQueryClient();
+  const { id: communityId } = params;
 
   return (
     <ScreenContainer isSignedOutModal>
       <HydrationBoundary state={dehydrate(queryClient)}>
-        <EditCommunityForm communityId="7" />
+        <EditCommunityForm communityId={communityId} />
       </HydrationBoundary>
     </ScreenContainer>
   );
