21d570c6e037e805cb456d7e4f88f8c1f8edaab0
moonia <mounia.arjdal@epitech.eu>
2025-09-22 21:47:36 +0700

    fix: rm roles field name

--- components/user/community-member-card.tsx
+++ components/user/community-member-card.tsx
@@ -31,17 +31,12 @@ function CommunityMemberCard({
             {address.substring(0, 10)}
           </Text>
         </div>
-        <div className="flex flex-col gap-2">
-          <Text size="sm" className="font-semibold text-primary">
-            Roles
-          </Text>
-          <div className="flex gap-2 flex-wrap">
-            {roles.map((role) => (
-              <Badge key={role} variant="outline" className="text-sm px-3 py-1">
-                {role}
-              </Badge>
-            ))}
-          </div>
+        <div className="flex gap-2 flex-wrap">
+          {roles.map((role) => (
+            <Badge key={role} variant="outline" className="text-sm px-3 py-1">
+              {role}
+            </Badge>
+          ))}
         </div>
       </div>
     </Card>
