5dd5662c9dce527c4404f9ee0777dd300f8ba9e6
Norman <norman@samourai.coop>
2025-09-27 19:28:43 +0200

    fix: same rounding
    Signed-off-by: Norman <norman@samourai.coop>

--- components/community/community-card.tsx
+++ components/community/community-card.tsx
@@ -42,7 +42,7 @@ function CommunityCard({ id, community }: CommunityCardProps) {
               sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               33vw"
-              className="flex w-full rounded-xl md:rounded self-center object-cover"
+              className="flex w-full rounded self-center object-cover"
             />
           </AspectRatio>
         </div>
