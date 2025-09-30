da263362858f7d194760caa38982b43267f603ed
Norman <norman@samourai.coop>
2025-09-30 15:57:39 +0200

    chore: better otel setup maybe
    Signed-off-by: Norman <norman@samourai.coop>

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,9 @@
 import * as Sentry from "@sentry/nextjs";
+import { registerOTel } from "@vercel/otel";
 
 export async function register() {
+  registerOTel({ serviceName: "zenao" });
+
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
   }
