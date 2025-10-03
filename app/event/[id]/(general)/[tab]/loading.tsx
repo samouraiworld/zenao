da2cca9cae727508aa0ef2db31a69db0ba44770f
Norman <norman@samourai.coop>
2025-10-03 20:03:02 +0200

    chore: remove tutorial comment
    Signed-off-by: Norman <norman@samourai.coop>

--- app/event/[id]/(general)/[tab]/loading.tsx
+++ app/event/[id]/(general)/[tab]/loading.tsx
@@ -2,7 +2,6 @@ import { EventInfoLayoutSkeleton } from "./event-info-layout";
 import { ScreenContainer } from "@/components/layout/screen-container";
 
 export default function Loading() {
-  // Or a custom loading skeleton component
   return (
     <ScreenContainer>
       <EventInfoLayoutSkeleton />
