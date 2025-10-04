7cddbf8156afad9e893848670e053ec21de7606d
Norman <norman@samourai.coop>
2025-10-04 03:12:40 +0200

    feat: add graphic loader for tabs content
    Signed-off-by: Norman <norman@samourai.coop>

--- app/event/[id]/@tabs/loading.tsx
+++ app/event/[id]/@tabs/loading.tsx
@@ -1,3 +1,9 @@
+import { Loader2 } from "lucide-react";
+
 export default function Loading() {
-  return null;
+  return (
+    <div className="flex justify-center">
+      <Loader2 />
+    </div>
+  );
 }
