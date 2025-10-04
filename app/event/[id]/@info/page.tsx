eb6d5ecd0d5e554be5da3cc047bd0a96332a727d
Norman <norman@samourai.coop>
2025-10-04 02:03:50 +0200

    chore: try with hydration boundary
    Signed-off-by: Norman <norman@samourai.coop>

--- app/event/[id]/@info/page.tsx
+++ app/event/[id]/@info/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { EventInfoLayout } from "./event-info-layout";
 import { eventOptions } from "@/lib/queries/event";
 import { getQueryClient } from "@/lib/get-query-client";
@@ -12,7 +13,13 @@ type Props = {
 
 export default async function EventPage({ params }: Props) {
   const { id } = await params;
-  return <EventInfoLayout eventId={id} />;
+  const queryClient = getQueryClient();
+  queryClient.prefetchQuery(eventOptions(id));
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <EventInfoLayout eventId={id} />
+    </HydrationBoundary>
+  );
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
