d2f561c201128b9373028d74e19d123ec4225bf3
Norman <norman@samourai.coop>
2025-10-04 03:30:54 +0200

    chore: revert since it creates a ghost page /description
    Signed-off-by: Norman <norman@samourai.coop>

--- app/event/[id]/@tabs/page.tsx
+++ app/event/[id]/@tabs/page.tsx
@@ -1,3 +1,17 @@
-import Page from "./description/page";
+import { eventOptions } from "@/lib/queries/event";
+import { getQueryClient } from "@/lib/get-query-client";
+import { MarkdownPreview } from "@/components/widgets/markdown-preview";
 
-export default Page;
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function DescriptionPage({ params }: Props) {
+  const { id } = await params;
+
+  const queryClient = getQueryClient();
+
+  const data = await queryClient.fetchQuery(eventOptions(id));
+
+  return <MarkdownPreview markdownString={data.description} />;
+}
