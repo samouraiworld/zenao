0c2a6fabd67691a1aa2c1e45071a73aecb0f8275
moonia <mounia.arjdal@epitech.eu>
2025-10-08 10:13:47 +0700

    fix: check-js ci

--- app/profile/[address]/profile-header.tsx
+++ app/profile/[address]/profile-header.tsx
@@ -48,15 +48,15 @@ export default function ProfileHeader({
   return (
     <div className="flex flex-col w-full">
       <div className="relative w-full">
-          <AspectRatio ratio={4 / 1}>
-            <Web3Image
-              src={profileDetails?.bannerUri || "/zenao-banner.png"}
-              alt="Profile banner"
-              priority
-              fill
-              className="w-full h-full object-cover"
-            />
-          </AspectRatio>
+        <AspectRatio ratio={4 / 1}>
+          <Web3Image
+            src={profileDetails?.bannerUri || "/zenao-banner.png"}
+            alt="Profile banner"
+            priority
+            fill
+            className="w-full h-full object-cover"
+          />
+        </AspectRatio>
 
         <div className="absolute -bottom-14 left-6">
           {avatarUri ? (
