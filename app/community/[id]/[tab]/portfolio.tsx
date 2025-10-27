0fed29faed793e99ed58f246fa45b00e62f062b4
Mio-san <twilson.freelance@gmail.com>
2025-10-28 00:54:25 +0900

    feat(front): explicit file size limit on upload (#847)
    Fix #846 
    <img width="926" height="497" alt="Screenshot 2025-10-24 at 14 21 22"
    src="https://github.com/user-attachments/assets/f1609b4e-581f-4daf-bdc1-b62ff770d7a3"
    />
    
    ---------
    
    Co-authored-by: MikaelVallenet <mikael.vallenetpro@gmail.com>
    Co-authored-by: David <60177543+Davphla@users.noreply.github.com>

--- app/community/[id]/[tab]/portfolio.tsx
+++ app/community/[id]/[tab]/portfolio.tsx
@@ -36,6 +36,12 @@ import {
 } from "@/components/shadcn/tooltip";
 import PortfolioUploadVideoDialog from "@/components/dialogs/portfolio-upload-video-dialog";
 import { MarkdownPreview } from "@/components/widgets/markdown-preview";
+import {
+  AUDIO_FILE_SIZE_LIMIT,
+  AUDIO_FILE_SIZE_LIMIT_MB,
+  IMAGE_FILE_SIZE_LIMIT,
+  IMAGE_FILE_SIZE_LIMIT_MB,
+} from "@/app/event/[id]/constants";
 
 type CommunityPortfolioProps = {
   communityId: string;
@@ -155,7 +161,10 @@ export default function CommunityPortfolio({
 
       setIsUploading(true);
 
-      const uri = await uploadFile(files[0]);
+      const uri = await uploadFile(
+        files[0],
+        fileType === "image" ? IMAGE_FILE_SIZE_LIMIT : AUDIO_FILE_SIZE_LIMIT,
+      );
       const newItem: PortfolioItem = {
         type: fileType,
         name: files[0].name,
@@ -173,10 +182,26 @@ export default function CommunityPortfolio({
     } catch (error) {
       captureException(error);
       console.error("Upload failed", error);
-      toast({
-        title: t("error-file-uploading"),
-        variant: "destructive",
-      });
+
+      if (
+        error instanceof Error &&
+        error.message.includes("File size exceeds limit")
+      ) {
+        toast({
+          variant: "destructive",
+          title: t("error-filesize-exceeds-limit", {
+            size:
+              fileType === "image"
+                ? IMAGE_FILE_SIZE_LIMIT_MB
+                : AUDIO_FILE_SIZE_LIMIT_MB,
+          }),
+        });
+      } else {
+        toast({
+          title: t("error-file-uploading"),
+          variant: "destructive",
+        });
+      }
     } finally {
       setIsUploading(false);
       if (imageFileInputRef.current) {
