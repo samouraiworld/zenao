62d2200cce0e1cfa2568f14eb4e40aff2fbe6353
moonia <99096342+moonia@users.noreply.github.com>
2025-11-11 07:32:46 +0100

    fix(front): community banner aspect ratio (#848)

--- components/features/community/community-form.tsx
+++ components/features/community/community-form.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { UseFormReturn, useFieldArray, useWatch } from "react-hook-form";
 import { useTranslations } from "next-intl";
 import { Trash2Icon, Plus, Info } from "lucide-react";
-import { useMediaQuery } from "../../../hooks/use-media-query";
 import SocialMediaLinks from "../user/settings/social-media-links";
 import { Form } from "@/components/shadcn/form";
 import { FormFieldInputString } from "@/components/widgets/form/form-field-input-string";
@@ -50,7 +49,6 @@ export const CommunityForm = ({
   });
 
   const description = form.watch("description");
-  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   const lastAdmin = adminInputs?.[adminInputs.length - 1];
   const isLastAdminInvalid =
@@ -72,7 +70,7 @@ export const CommunityForm = ({
           <FormFieldImage
             name="bannerUri"
             control={form.control}
-            aspectRatio={isDesktop ? [48, 9] : [21, 9]}
+            aspectRatio={[4, 1]}
             fileSizeLimitMb={IMAGE_FILE_SIZE_LIMIT}
             placeholder={t("upload-banner")}
             className="w-full rounded-xl overflow-hidden"
