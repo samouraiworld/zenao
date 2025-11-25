322ce885ccf834129f8a2ce850b699c8e754b463
miosan <miosan_desu@proton.me>
2025-11-25 15:58:10 +0900

    add loading.tsx communities + expand suspense boundaries

--- app/communities/layout.tsx
+++ app/communities/layout.tsx
@@ -5,6 +5,8 @@ import {
   communitiesList,
   DEFAULT_COMMUNITIES_LIMIT,
 } from "@/lib/queries/community";
+import Heading from "@/components/widgets/texts/heading";
+import { GnowebButton } from "@/components/widgets/buttons/gnoweb-button";
 
 export const revalidate = 600;
 
@@ -19,7 +21,21 @@ function CommunitiesLayout({ children }: CommunitiesLayoutProps) {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <ScreenContainer>{children}</ScreenContainer>
+      <ScreenContainer>
+        <div className="flex flex-col gap-12 mb-3">
+          <div className="flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between md:items-center">
+            <div className="flex flex-col md:flex-row gap-2 md:items-center">
+              <Heading level={1} size="4xl" className="truncate">
+                Communities
+              </Heading>
+              <GnowebButton
+                href={`${process.env.NEXT_PUBLIC_GNOWEB_URL}/r/zenao/communityreg`}
+              />
+            </div>
+          </div>
+          {children}
+        </div>
+      </ScreenContainer>
     </HydrationBoundary>
   );
 }
