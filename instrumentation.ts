65b79a59f5d8089cd5f4f17ce351cfb697e01e84
Mio-san <twilson.freelance@gmail.com>
2025-11-26 13:06:41 +0900

    feat(front): Query profiling (#883)
    Fix #880 
    
    Able to collect metrics from queries on Frontend
    
    <img width="1512" height="945" alt="Screenshot 2025-11-13 at 17 57 07"
    src="https://github.com/user-attachments/assets/37503872-4235-4463-9c96-05c835f293d9"
    />
    
    ---------
    
    Co-authored-by: MikaelVallenet <mikael.vallenetpro@gmail.com>

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,17 @@
 import * as Sentry from "@sentry/nextjs";
+import { OTLPHttpJsonTraceExporter, registerOTel } from "@vercel/otel";
 
 export async function register() {
+  registerOTel({
+    serviceName: "zenao:frontend",
+    traceExporter: new OTLPHttpJsonTraceExporter({
+      url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+      headers: {
+        "X-OTEL-TOKEN": process.env.OTEL_EXPORTER_OTLP_HEADERS_TOKEN,
+      },
+    }),
+  });
+
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
   }
