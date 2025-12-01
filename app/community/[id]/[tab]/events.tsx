e0f068158f0867c888ab4430a0c248fb710bd380
miosan <miosan_desu@proton.me>
2025-12-01 14:47:44 +0900

    community-events: display events descending (furthest first)

--- app/community/[id]/[tab]/events.tsx
+++ app/community/[id]/[tab]/events.tsx
@@ -6,6 +6,7 @@ import {
   useSuspenseQuery,
   UseSuspenseQueryResult,
 } from "@tanstack/react-query";
+import { useMemo } from "react";
 import EmptyList from "@/components/widgets/lists/empty-list";
 import Heading from "@/components/widgets/texts/heading";
 import { communityUsersWithRoles } from "@/lib/queries/community";
@@ -14,6 +15,12 @@ import { eventIdFromPkgPath, eventOptions } from "@/lib/queries/event";
 import { EventInfo } from "@/app/gen/zenao/v1/zenao_pb";
 import EventCardListLayout from "@/components/features/event/event-card-list-layout";
 import { EventCard } from "@/components/features/event/event-card";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/shadcn/tabs";
 
 type CommunityEventsProps = {
   communityId: string;
@@ -42,31 +49,79 @@ function CommunityEvents({ communityId, now: _now }: CommunityEventsProps) {
         })),
   });
 
+  const sortedEvents = useMemo(() => {
+    return events.sort((a, b) => {
+      return Number(b.startDate) - Number(a.startDate);
+    });
+  }, [events]);
+
+  const upcomingEvents = useMemo(
+    () =>
+      sortedEvents.filter((evt) => {
+        return Number(evt.endDate) * 1000 >= BigInt(Date.now());
+      }),
+    [sortedEvents],
+  );
+
+  const pastEvents = useMemo(
+    () =>
+      sortedEvents.filter((evt) => {
+        return Number(evt.endDate) * 1000 < BigInt(Date.now());
+      }),
+    [sortedEvents],
+  );
+
   return (
     <div className="space-y-8 pb-8">
       <div className="flex flex-col gap-4">
-        <Heading level={2} size="lg">
-          {t("community.events")}
-        </Heading>
-
-        <div className="flex flex-col gap-0">
-          {events.length === 0 && (
-            <EmptyList
-              title={t("no-events-title")}
-              description={t("no-events-description")}
-            />
-          )}
-        </div>
-
-        <EventCardListLayout>
-          {events.map((evt) => (
-            <EventCard
-              key={evt.pkgPath}
-              evt={evt}
-              href={`/event/${eventIdFromPkgPath(evt.pkgPath)}`}
-            />
-          ))}
-        </EventCardListLayout>
+        <Tabs defaultValue="upcoming">
+          <TabsList>
+            <TabsTrigger value="upcoming">
+              {t("community.upcoming")}
+            </TabsTrigger>
+            <TabsTrigger value="past">{t("community.past")}</TabsTrigger>
+          </TabsList>
+          <TabsContent value="upcoming">
+            <div className="flex flex-col gap-0">
+              {upcomingEvents.length === 0 && (
+                <EmptyList
+                  title={t("no-events-title")}
+                  description={t("no-events-description")}
+                />
+              )}
+            </div>
+            <EventCardListLayout>
+              {upcomingEvents.map((evt) => (
+                <EventCard
+                  key={evt.pkgPath}
+                  evt={evt}
+                  href={`/event/${eventIdFromPkgPath(evt.pkgPath)}`}
+                  fullDate
+                />
+              ))}
+            </EventCardListLayout>
+          </TabsContent>
+          <TabsContent value="past">
+            <div className="flex flex-col gap-0">
+              {pastEvents.length === 0 && (
+                <EmptyList
+                  title={t("no-events-title")}
+                  description={t("no-events-description")}
+                />
+              )}
+            </div>
+            <EventCardListLayout>
+              {pastEvents.map((evt) => (
+                <EventCard
+                  key={evt.pkgPath}
+                  evt={evt}
+                  href={`/event/${eventIdFromPkgPath(evt.pkgPath)}`}
+                  fullDate
+                />
+              ))}
+            </EventCardListLayout>
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
