5f9bf30550f23379d2a0eac79c7a149e328cb1a2
MikaelVallenet <mikael.vallenetpro@gmail.com>
2025-12-08 16:21:52 +0100

    feat(back): add entity roles API route

--- backend/entity_roles.go
+++ backend/entity_roles.go
@@ -6,8 +6,29 @@ import (
 
 	"connectrpc.com/connect"
 	zenaov1 "github.com/samouraiworld/zenao/backend/zenao/v1"
+	"github.com/samouraiworld/zenao/backend/zeni"
 )
 
 func (s *ZenaoServer) EntityRoles(ctx context.Context, req *connect.Request[zenaov1.EntityRolesRequest]) (*connect.Response[zenaov1.EntityRolesResponse], error) {
-	return nil, errors.New("not implemented")
+	if req.Msg.Entity == nil || req.Msg.Entity.EntityType == "" || req.Msg.Entity.EntityId == "" {
+		return nil, errors.New("entity type and id are required")
+	}
+
+	if req.Msg.Org == nil || req.Msg.Org.EntityId == "" || req.Msg.Org.EntityType == "" {
+		return nil, errors.New("org entity type and id are required")
+	}
+
+	var roles []string
+	if err := s.DB.TxWithSpan(ctx, "EntityRoles", func(tx zeni.DB) error {
+		var err error
+		roles, err = tx.EntityRoles(req.Msg.Entity.EntityType, req.Msg.Entity.EntityId, req.Msg.Org.EntityType, req.Msg.Org.EntityId)
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return connect.NewResponse(&zenaov1.EntityRolesResponse{Roles: roles}), nil
 }
