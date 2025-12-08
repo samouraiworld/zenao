c346ac57fede276b22cd807c86428facf7aa68a9
MikaelVallenet <mikael.vallenetpro@gmail.com>
2025-12-08 16:35:00 +0100

    feat(back): add users with roles API route

--- backend/entities_with_role.go
+++ backend/entities_with_role.go
@@ -5,10 +5,12 @@ import (
 	"errors"
 
 	"connectrpc.com/connect"
+	"github.com/samouraiworld/zenao/backend/mapsl"
 	zenaov1 "github.com/samouraiworld/zenao/backend/zenao/v1"
+	"github.com/samouraiworld/zenao/backend/zeni"
 )
 
-func (s *ZenaoServer) EntitiesWithRole(ctx context.Context, req *connect.Request[zenaov1.EntitiesWithRoleRequest]) (*connect.Response[zenaov1.EntitiesWithRoleResponse], error) {
+func (s *ZenaoServer) UsersWithRole(ctx context.Context, req *connect.Request[zenaov1.UsersWithRoleRequest]) (*connect.Response[zenaov1.UsersWithRoleResponse], error) {
 	if req.Msg.Org == nil || req.Msg.Org.EntityId == "" || req.Msg.Org.EntityType == "" {
 		return nil, errors.New("org entity type and id are required")
 	}
@@ -17,5 +19,21 @@ func (s *ZenaoServer) EntitiesWithRole(ctx context.Context, req *connect.Request
 		return nil, errors.New("role is required")
 	}
 
-	return nil, errors.New("not implemented")
+	var users []*zeni.User
+	if err := s.DB.TxWithSpan(ctx, "UsersWithRole", func(tx zeni.DB) error {
+		var err error
+		users, err = tx.GetOrgUsersWithRole(req.Msg.Org.EntityType, req.Msg.Org.EntityId, req.Msg.Role)
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	usersIDs := mapsl.Map(users, func(u *zeni.User) string {
+		return u.ID
+	})
+
+	return connect.NewResponse(&zenaov1.UsersWithRoleResponse{UsersIds: usersIDs}), nil
 }
