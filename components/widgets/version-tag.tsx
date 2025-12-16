142ba9f7b77453d45f51b6931a507d108b4662b1
miosan <miosan_desu@proton.me>
2025-12-16 11:53:53 +0900

    not packageJson exposure

--- components/widgets/version-tag.tsx
+++ components/widgets/version-tag.tsx
@@ -1,4 +1,3 @@
-import packageJson from "../../package.json";
 import { Badge } from "../shadcn/badge";
 import Text from "./texts/text";
 import { cn } from "@/lib/tailwind";
@@ -7,7 +6,8 @@ export default function VersionTag({ className }: { className?: string }) {
   return (
     <Badge variant="secondary" className={cn("rounded", className)}>
       <Text size="sm" variant="secondary">
-        Beta <span className="hidden sm:inline">{packageJson.version}</span>
+        Beta{" "}
+        <span className="hidden sm:inline">{process.env.NEXT_APP_VERSION}</span>
       </Text>
     </Badge>
   );
