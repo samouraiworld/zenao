29fbfe31fa26cc60105f38a92696e4a76ca5ffef
miosan <miosan_desu@proton.me>
2025-12-16 18:26:05 +0900

    add creation logic form + update tracking events

--- app/community/(settings)/create/create-community-form.tsx
+++ app/community/(settings)/create/create-community-form.tsx
@@ -4,6 +4,8 @@ import { useAuth } from "@clerk/nextjs";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useTranslations } from "next-intl";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import {
   CommunityDetails,
   communityFormSchema,
@@ -13,10 +15,18 @@ import { CommunityForm } from "@/components/features/community/community-form";
 import { captureException } from "@/lib/report";
 import { useToast } from "@/hooks/use-toast";
 import { serializeWithFrontMatter } from "@/lib/serialization";
+import { useCreateCommunity } from "@/lib/mutations/community-create";
+import { userInfoOptions } from "@/lib/queries/user";
+import { useAnalyticsEvents } from "@/hooks/use-analytics-events";
 
 export default function CreateCommunityForm() {
   const { toast } = useToast();
+  const { trackEvent } = useAnalyticsEvents();
+  const router = useRouter();
   const { userId, getToken } = useAuth();
+  const { data: userInfo } = useSuspenseQuery(
+    userInfoOptions(getToken, userId),
+  );
 
   const t = useTranslations("community-form");
   const form = useForm<CommunityFormSchemaType>({
@@ -33,13 +43,13 @@ export default function CreateCommunityForm() {
     },
   });
 
-  // const { createCommunity, isPending } = useCreateCommunity();
+  const { createCommunity, isPending } = useCreateCommunity();
 
   const onSubmit = async (data: CommunityFormSchemaType) => {
     try {
       const token = await getToken();
 
-      if (!token || !userId) {
+      if (!token || !userId || !userInfo?.realmId) {
         throw new Error("User is not authenticated");
       }
 
@@ -53,22 +63,38 @@ export default function CreateCommunityForm() {
 
       console.log("Creating community with data:", data);
 
-      // const communityId = await createCommunity({
-      //   token,
-      //   displayName: data.displayName,
-      //   avatarUri: data.avatarUri,
-      //   bannerUri: data.bannerUri,
-      //   description,
-      //   administrators: data.administrators,
-      // });
+      const communityId = await createCommunity({
+        token,
+        userRealmId: userInfo?.realmId,
+        displayName: data.displayName,
+        avatarUri: data.avatarUri,
+        bannerUri: data.bannerUri,
+        description,
+        administrators: data.administrators.map((admin) => admin.address),
+      });
+
+      trackEvent("CommunityCreated", {
+        props: {
+          communityId,
+        },
+      });
+
+      form.reset();
+      toast({
+        title: t("toast-creation-success"),
+      });
+
+      router.push(`/community/${communityId}`);
     } catch (error) {
       captureException(error);
       toast({
         variant: "destructive",
-        title: t("update-failure"),
+        title: t("toast-creation-failure"),
       });
     }
   };
 
-  return <CommunityForm form={form} onSubmit={onSubmit} isLoading={false} />;
+  return (
+    <CommunityForm form={form} onSubmit={onSubmit} isLoading={isPending} />
+  );
 }
