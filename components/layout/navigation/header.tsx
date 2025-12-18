2ef960d4eeaf4ace4c1b43ec8aff7ae812c9c29e
miosan <miosan_desu@proton.me>
2025-12-18 16:08:19 +0900

    header: add blog link

--- components/layout/navigation/header.tsx
+++ components/layout/navigation/header.tsx
@@ -10,6 +10,7 @@ import {
   BoxesIcon,
   CompassIcon,
   LucideProps,
+  Rss,
   Tickets,
 } from "lucide-react";
 import {
@@ -89,6 +90,13 @@ const HeaderLinks = () => {
         icon: BookOpenText,
         needsAuth: false,
         children: t("manifesto"),
+      },
+      {
+        key: "blog",
+        to: "/blog",
+        icon: Rss,
+        needsAuth: false,
+        children: t("blog"),
       },
     ],
     [t],
