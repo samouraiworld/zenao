715e1de9cb9bc9d296c7e370dc4427ab73cdec26
WaDadidou <wadadidou@gmail.com>
2025-12-19 00:00:35 +0100

    fix: small adjust dashboard header/sidebar

--- components/features/dashboard/header.tsx
+++ components/features/dashboard/header.tsx
@@ -18,14 +18,14 @@ export default function DashboardHeader({ navbarStyle }: DashboardHeaderProps) {
     <header
       data-navbar-style={navbarStyle}
       className={cn(
-        "flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12",
+        "flex py-2 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:py-2",
         // Handle sticky navbar style with conditional classes so blur, background, z-index, and rounded corners remain consistent across all SidebarVariant layouts.
         "data-[navbar-style=sticky]:bg-background/50 data-[navbar-style=sticky]:sticky data-[navbar-style=sticky]:top-0 data-[navbar-style=sticky]:z-50 data-[navbar-style=sticky]:overflow-hidden data-[navbar-style=sticky]:rounded-t-[inherit] data-[navbar-style=sticky]:backdrop-blur-md",
       )}
     >
-      <div className="flex w-full items-center justify-between px-4 lg:px-6">
+      <div className="flex w-full items-center justify-between px-4 lg:px-4">
         <div className="flex items-center gap-1 lg:gap-2">
-          <SidebarTrigger className="-ml-1" />
+          <SidebarTrigger />
           <Separator
             orientation="vertical"
             className="mx-2 data-[orientation=vertical]:h-4"
