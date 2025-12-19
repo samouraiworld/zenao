ad10e77819b6a0c53f6f778a7a7e9f47e2e19686
WaDadidou <wadadidou@gmail.com>
2025-12-19 18:02:14 +0100

    fix: remove tooltip

--- components/widgets/portfolio/upload-medias-buttons.tsx
+++ components/widgets/portfolio/upload-medias-buttons.tsx
@@ -1,11 +1,6 @@
 import { useTranslations } from "next-intl";
 import { AudioWaveform, ImageIcon, Video } from "lucide-react";
 import { Dispatch, RefObject, SetStateAction } from "react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/shadcn/tooltip";
 import { Button } from "@/components/shadcn/button";
 import { cn } from "@/lib/tailwind";
 
@@ -28,50 +23,35 @@ export function UploadMediasButtons({
     <div
       className={cn("flex justify-center gap-4 h-12 items-center", className)}
     >
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button
-            variant="outline"
-            className="text-main bg-transparent border-none"
-            disabled={isUploading}
-            onClick={() => imageFileInputRef.current?.click()}
-          >
-            <ImageIcon className="!size-6" />
-            <span className="max-sm:hidden">{t("upload-image")}</span>
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>{t("upload-image")}</TooltipContent>
-      </Tooltip>
+      <Button
+        variant="outline"
+        className="text-main bg-transparent border-none"
+        disabled={isUploading}
+        onClick={() => imageFileInputRef.current?.click()}
+      >
+        <ImageIcon className="!size-6" />
+        <span className="max-sm:hidden">{t("upload-image")}</span>
+      </Button>
 
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button
-            variant="outline"
-            className="text-main bg-transparent border-none"
-            disabled={isUploading}
-            onClick={() => audioFileInputRef.current?.click()}
-          >
-            <AudioWaveform className="!size-6" />
-            <span className="max-sm:hidden">{t("upload-audio")}</span>
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>{t("upload-audio")}</TooltipContent>
-      </Tooltip>
+      <Button
+        variant="outline"
+        className="text-main bg-transparent border-none"
+        disabled={isUploading}
+        onClick={() => audioFileInputRef.current?.click()}
+      >
+        <AudioWaveform className="!size-6" />
+        <span className="max-sm:hidden">{t("upload-audio")}</span>
+      </Button>
 
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button
-            variant="outline"
-            className="text-main bg-transparent border-none"
-            disabled={isUploading}
-            onClick={() => setVideoDialogOpen(true)}
-          >
-            <Video className="!size-6" />
-            <span className="max-sm:hidden">{t("upload-video")}</span>
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>{t("upload-video")}</TooltipContent>
-      </Tooltip>
+      <Button
+        variant="outline"
+        className="text-main bg-transparent border-none"
+        disabled={isUploading}
+        onClick={() => setVideoDialogOpen(true)}
+      >
+        <Video className="!size-6" />
+        <span className="max-sm:hidden">{t("upload-video")}</span>
+      </Button>
     </div>
   );
 }
