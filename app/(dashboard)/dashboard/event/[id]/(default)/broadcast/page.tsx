60ea1f9d08717b3b0249788f3f6d211b8a4e7c63
miosan <miosan_desu@proton.me>
2025-12-22 11:32:48 +0900

    dashboard-event-details: broadcast page

--- app/(dashboard)/dashboard/event/[id]/(default)/broadcast/page.tsx
+++ app/(dashboard)/dashboard/event/[id]/(default)/broadcast/page.tsx
@@ -1,9 +1,38 @@
+import { notFound } from "next/navigation";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { getTranslations } from "next-intl/server";
+import DashboardBroadcastForm from "./dashboard-broadcast-form";
+import { getQueryClient } from "@/lib/get-query-client";
+import { eventOptions } from "@/lib/queries/event";
+import Text from "@/components/widgets/texts/text";
+
 interface DashboardEventBroadcastPageProps {
   params: Promise<{ id: string }>;
 }
 
-export default function DashboardEventBroadcastPage({
-  params: _,
+export default async function DashboardEventBroadcastPage({
+  params,
 }: DashboardEventBroadcastPageProps) {
-  return <div>Broadcast Page</div>;
+  const { id: eventId } = await params;
+
+  const queryClient = getQueryClient();
+
+  const t = await getTranslations("broadcast-email-form");
+
+  let eventInfo;
+
+  try {
+    eventInfo = await queryClient.fetchQuery(eventOptions(eventId));
+  } catch {
+    notFound();
+  }
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="mb-12 space-y-8">
+        <Text variant="secondary">{t("desc")}</Text>
+        <DashboardBroadcastForm eventId={eventId} eventInfo={eventInfo} />
+      </div>
+    </HydrationBoundary>
+  );
 }
