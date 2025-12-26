b504e7e486221f7e36ba0ac19fba2672987fbc5f
miosan <miosan_desu@proton.me>
2025-12-26 19:09:00 +0900

    dashboard(event-details): update loading components

--- app/(dashboard)/dashboard/event/[id]/(default)/gatekeepers/loading.tsx
+++ app/(dashboard)/dashboard/event/[id]/(default)/gatekeepers/loading.tsx
@@ -1,3 +1,9 @@
-export default function DashboardEventParticipantsLoadingPage() {
-  return <div>Loading gatekeepers...</div>;
+import { Loader2 } from "lucide-react";
+
+export default function DashboardEventGatekeepersLoadingPage() {
+  return (
+    <div className="flex w-full h-12 justify-center items-center">
+      <Loader2 className="animate-spin h-6 w-6" />
+    </div>
+  );
 }
--- app/(dashboard)/dashboard/event/[id]/(default)/participants/loading.tsx
+++ app/(dashboard)/dashboard/event/[id]/(default)/participants/loading.tsx
@@ -1,3 +1,9 @@
+import { Loader2 } from "lucide-react";
+
 export default function DashboardEventParticipantsLoadingPage() {
-  return <div>Loading participants...</div>;
+  return (
+    <div className="flex w-full h-12 justify-center items-center">
+      <Loader2 className="animate-spin h-6 w-6" />
+    </div>
+  );
 }
