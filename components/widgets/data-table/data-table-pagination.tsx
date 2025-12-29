2c258d32e75f239196eba92673e392ec2634697e
miosan <miosan_desu@proton.me>
2025-12-29 15:38:43 +0900

    i18n data table pagination

--- components/widgets/data-table/data-table-pagination.tsx
+++ components/widgets/data-table/data-table-pagination.tsx
@@ -6,6 +6,7 @@ import {
   ChevronsRight,
 } from "lucide-react";
 
+import { useTranslations } from "next-intl";
 import { Button } from "@/components/shadcn/button";
 import { Label } from "@/components/shadcn/label";
 import {
@@ -38,12 +39,14 @@ export function DataTablePaginationSync<TData>({
   table,
   pagination,
 }: DataTablePaginationProps<TData>) {
+  const t = useTranslations("data-table.pagination");
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex w-full items-center gap-8 lg:w-fit">
         <div className="hidden items-center gap-2 lg:flex">
           <Label htmlFor="rows-per-page" className="text-sm font-medium">
-            Rows per page
+            {t("rows-per-page")}
           </Label>
           <Select
             value={`${table.getState().pagination.pageSize}`}
@@ -68,8 +71,10 @@ export function DataTablePaginationSync<TData>({
           (pagination?.page ?? 1) - 1 < table.getPageCount()) ??
           (0 < table.getPageCount() && (
             <div className="flex w-fit items-center justify-center text-sm font-medium">
-              Page {table.getState().pagination.pageIndex + 1} of{" "}
-              {table.getPageCount()}
+              {t("page-of", {
+                page: table.getState().pagination.pageIndex + 1,
+                pages: table.getPageCount(),
+              })}
             </div>
           ))}
         <div className="ml-auto flex items-center gap-2 lg:ml-0">
