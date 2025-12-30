d546c046c9fe1cfa1dc5cdfffcf572db2a972a9a
miosan <miosan_desu@proton.me>
2025-12-31 00:49:27 +0900

    blog post list page using seobot

--- app/(general)/blog/blog-post-card.tsx
+++ app/(general)/blog/blog-post-card.tsx
@@ -1,72 +1,104 @@
+"use client";
+
 import Link from "next/link";
+import { IArticleIndex } from "seobot/dist/types/blog";
+import { useRouter } from "next/navigation";
 import PostDate from "./[slug]/post-date";
 import { Card } from "@/components/widgets/cards/card";
 import Text from "@/components/widgets/texts/text";
 import Heading from "@/components/widgets/texts/heading";
 import { EventImage } from "@/components/features/event/event-image";
 
-interface BlogPostCardProps {
-  title: string;
-  description: string;
-  author: string;
-  category: string;
-  previewImageUrl?: string;
-  slug: string;
-  publishedAt: Date;
+// interface BlogPostCardProps {
+//   title: string;
+//   description: string;
+//   author: string;
+//   category: string;
+//   previewImageUrl?: string;
+//   slug: string;
+//   publishedAt: Date;
+// }
+
+declare module "seobot/dist/types/blog" {
+  interface IArticleIndex {
+    id: string;
+    slug: string;
+    headline: string;
+    metaDescription: string;
+    image: string;
+    readingTime: number;
+    publishedAt: string | null;
+    createdAt: string;
+    updatedAt: string;
+    category: {
+      title: string;
+      slug: string;
+    } | null;
+    tags: {
+      title: string;
+      slug: string;
+    }[];
+    isTool?: boolean;
+    isVideo?: boolean;
+    isNews?: boolean;
+  }
 }
 
-export default function BlogPostCard({
-  title,
-  author,
-  description,
-  category,
-  slug,
-  previewImageUrl,
-  publishedAt,
-}: BlogPostCardProps) {
-  const { alt, ...imageProps } = previewImageUrl
+export default function BlogPostCard({ post }: { post: IArticleIndex }) {
+  const router = useRouter();
+  const { alt, ...imageProps } = post.image
     ? {
-        src: previewImageUrl,
-        alt: `${title} post image background`,
+        src: post.image,
+        alt: `${post.headline} post image background`,
       }
     : {
         src: "/zenao-logo.png",
         alt: "Zenao logo",
       };
   return (
-    <Link href={`/blog/${slug}`} className="w-full h-full">
-      <Card
-        role="group"
-        className="flex flex-col h-full gap-8 hover:bg-secondary/50 transition-colors"
-      >
-        <div className="flex flex-col gap-2">
-          <Text size="sm" className="text-main font-semibold">
-            {category}
-          </Text>
-          <EventImage
-            {...imageProps}
-            alt={alt}
-            fill
-            sizes="(max-width: 768px) 100vw,
+    <Card
+      role="group"
+      className="cursor-pointer w-full flex flex-col h-full gap-8 hover:bg-secondary/50 transition-colors"
+      onClick={() => router.push(`/blog/${post.slug}`)}
+    >
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-wrap gap-2">
+          {post.category && (
+            <Link href={`/blog/category/${post.category.slug}`}>
+              <Text size="sm" className="text-main font-semibold">
+                {post.category.title}
+              </Text>
+            </Link>
+          )}
+        </div>
+
+        <EventImage
+          {...imageProps}
+          alt={alt}
+          fill
+          sizes="(max-width: 768px) 100vw,
                 (max-width: 1200px) 50vw,
                 33vw"
-            quality={60}
-          />
+          quality={60}
+        />
+      </div>
+      <div className="flex flex-col h-full gap-4">
+        <Heading level={2} size="xl" className="font-bold">
+          {post.headline}
+        </Heading>
+        <div className="flex-grow min-h-14">
+          <Text variant="secondary">{post.metaDescription}</Text>
         </div>
-        <div className="flex flex-col h-full gap-4">
-          <Heading level={2} size="xl" className="font-bold">
-            {title}
-          </Heading>
-          <div className="flex-grow min-h-14">
-            <Text variant="secondary">{description}</Text>
-          </div>
-          <div className="flex gap-2">
-            <Text className="font-semibold">{author}</Text>
-            <Text variant="secondary">•</Text>
-            <PostDate date={publishedAt} />
-          </div>
+        <div className="flex gap-2">
+          <PostDate date={new Date(post.publishedAt || post.createdAt)} />
+          {post.readingTime ? (
+            <>
+              <Text variant="secondary">•</Text>
+              <Text className="font-semibold">{post.readingTime} min read</Text>
+            </>
+          ) : null}
         </div>
-      </Card>
-    </Link>
+      </div>
+    </Card>
   );
 }
--- app/(general)/blog/page.tsx
+++ app/(general)/blog/page.tsx
@@ -1,18 +1,60 @@
 import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
+import { BlogClient } from "seobot";
 import BlogPostCard from "./blog-post-card";
 import BlogHeader from "./blog-header";
+import Pagination from "./blog-pagination";
 import { ScreenContainer } from "@/components/layout/screen-container";
-import { getPostsMetadata } from "@/lib/blog";
 
-export const metadata: Metadata = {
-  title: "Blog | Zenao",
-  description:
-    "Find the latest articles, news, and updates about Zenao in our blog.",
-};
+async function getPosts(page: number) {
+  const key = process.env.SEOBOT_API_KEY;
+  if (!key)
+    throw Error(
+      "SEOBOT_API_KEY enviroment variable must be set. You can use the DEMO key a8c58738-7b98-4597-b20a-0bb1c2fe5772 for testing - please set it in the root .env.local file",
+    );
 
-export default async function Blog() {
-  const posts = await getPostsMetadata();
+  const client = new BlogClient(key);
+  return client.getArticles(page, 10);
+}
+
+export const fetchCache = "force-no-store";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const title = "Blog | Zenao";
+  const description =
+    "Discover the latest articles, news, and updates about Zenao in our blog.";
+  return {
+    title,
+    description,
+    metadataBase: new URL("https://zenao.io"),
+    alternates: {
+      canonical: "/blog",
+    },
+    openGraph: {
+      type: "website",
+      title,
+      description,
+      // images: [],
+      url: "https://zenao.io/blog",
+    },
+    twitter: {
+      title,
+      description,
+    },
+  };
+}
+
+export default async function Blog({
+  searchParams: { page },
+}: {
+  searchParams: { page: number };
+}) {
+  const pageNumber = Math.max((page || 0) - 1, 0);
+  const { total, articles } = await getPosts(pageNumber);
+  const posts = articles || [];
+  const lastPage = Math.ceil(total / 10);
+
+  // const posts = await getPostsMetadata();
   const t = await getTranslations("blog");
 
   if (!posts.length) {
@@ -32,18 +74,17 @@ export default async function Blog() {
         <BlogHeader />
         <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
           {posts.map((post) => (
-            <BlogPostCard
-              key={post.slug}
-              slug={post.slug}
-              title={post.title}
-              author={post.author}
-              category={post.category}
-              description={post.description}
-              previewImageUrl={post.previewImageUrl}
-              publishedAt={new Date(post.date)}
-            />
+            <BlogPostCard key={post.slug} post={post} />
           ))}
         </div>
+
+        {lastPage > 1 && (
+          <Pagination
+            slug="/blog"
+            pageNumber={pageNumber}
+            lastPage={lastPage}
+          />
+        )}
       </div>
     </ScreenContainer>
   );
