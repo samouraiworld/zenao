42e3a3cbf68a3b8524b4245d4f283a26ed7c0f3f
miosan <miosan_desu@proton.me>
2026-01-06 12:34:31 +0900

    event-page: add banners to access daashboard according to your role

--- app/(general)/event/[id]/(general)/layout.tsx
+++ app/(general)/event/[id]/(general)/layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { EventInfoLayout } from "./event-info-layout";
@@ -6,6 +6,7 @@ import { EventScreenContainer } from "@/components/features/event/event-screen-c
 import { getQueryClient } from "@/lib/get-query-client";
 import { eventOptions } from "@/lib/queries/event";
 import { web2URL } from "@/lib/uris";
+import EventDashboardQuickAccessBanner from "@/components/features/event/event-dashboard-quick-access-banner";
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -46,6 +47,9 @@ export default async function EventLayout({ params, children }: Props) {
   return (
     <EventScreenContainer id={p.id}>
       <div className="flex flex-col gap-8">
+        <Suspense fallback={null}>
+          <EventDashboardQuickAccessBanner eventId={p.id} />
+        </Suspense>
         <EventInfoLayout eventId={p.id} data={event} />
         <div>{children}</div>
       </div>
