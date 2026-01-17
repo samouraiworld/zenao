54de1aaff1fd27af03830507bfc2706d7062e97c
WaDadidou <wadadidou@gmail.com>
2026-01-18 00:07:41 +0100

    fix: langage as middleware

--- app/i18n/request.ts
+++ app/i18n/request.ts
@@ -1,19 +1,14 @@
 import { getRequestConfig } from "next-intl/server";
-import { cookies } from "next/headers";
-import {
-  SUPPORTED_LOCALES,
-  DEFAULT_LOCALE,
-  LOCALE_COOKIE_NAME,
-  type Locale,
-} from "./config";
+import { headers } from "next/headers";
+import { SUPPORTED_LOCALES, DEFAULT_LOCALE, type Locale } from "./config";
 
 export default getRequestConfig(async () => {
-  const cookieStore = await cookies();
-  const localeCookie = cookieStore.get(LOCALE_COOKIE_NAME)?.value;
+  const headersList = await headers();
+  const localeHeader = headersList.get("x-locale");
 
   const locale =
-    localeCookie && SUPPORTED_LOCALES.includes(localeCookie as Locale)
-      ? (localeCookie as Locale)
+    localeHeader && SUPPORTED_LOCALES.includes(localeHeader as Locale)
+      ? (localeHeader as Locale)
       : DEFAULT_LOCALE;
 
   return {
